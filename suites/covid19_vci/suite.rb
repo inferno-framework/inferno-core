c90aa6e7ee015db9436771be02fbfc1d485b5780
Stephen MacVicar <smacvicar@mitre.org>
2021-05-18 08:07:12 -0400

    update suite title

--- suites/covid19_vci/suite.rb
+++ suites/covid19_vci/suite.rb
@@ -3,7 +3,7 @@ require 'health_cards'
 module Covid19VCI
   class Suite < Inferno::TestSuite
     id 'c19-vci'
-    title 'COVID-19 Vaccine Credential'
+    title 'SMART Health Cards: Vaccination & Testing'
 
     group from: :vci_file_download
     group from: :vci_fhir_operation
