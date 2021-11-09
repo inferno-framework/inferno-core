9561ced5b5d5d753e29616225035233cba13201c
Alyssa Wang <awang@mitre.org>
2021-11-09 16:37:07 -0500

    update lint rules for appbar

--- client/src/components/Header/styles.tsx
+++ client/src/components/Header/styles.tsx
@@ -1,9 +1,11 @@
+// Necessary to override default z-index value of appbar
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
 export default makeStyles((theme: Theme) => ({
   appbar: {
-    // Necessary to override default z-index value of appbar
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     zIndex: `${theme.zIndex.drawer + 1} !important` as any,
   },
   logo: {
