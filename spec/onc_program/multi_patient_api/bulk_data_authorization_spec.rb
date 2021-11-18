62fa291b58cdc11b472a28549ca6fc7d351009ca
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2021-11-18 13:53:13 -0500

    Removed TLS attempt. New ticket for TLS tester implementation on jira.

--- spec/onc_program/multi_patient_api/bulk_data_authorization_spec.rb
+++ spec/onc_program/multi_patient_api/bulk_data_authorization_spec.rb
@@ -2,5 +2,13 @@ require_relative '../../../dev_suites/ui_onc_program/multi_patient_api/bulk_data
 
 RSpec.describe ONCProgram::BulkDataAuthorization do 
 
-  
+  # TODO: After TLS tester class is implemented, create this test 
+  describe 'endpoint TLS tests' do
+
+  end 
+
+  describe 'require grant type' do
+    
+  end 
+
 end 
