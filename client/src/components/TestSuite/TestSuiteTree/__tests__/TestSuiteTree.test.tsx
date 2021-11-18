034ff9c7ee39b1fc812f1c2c96fa1fbb5709db2d
Alyssa Wang <awang@mitre.org>
2021-11-18 17:20:21 -0500

    fix test rendering

--- client/src/components/TestSuite/TestSuiteTree/__tests__/TestSuiteTree.test.tsx
+++ client/src/components/TestSuite/TestSuiteTree/__tests__/TestSuiteTree.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Test, TestGroup, TestSuite, RunnableType } from 'models/testSuiteModels';
 import TestSuiteTree, { TestSuiteTreeProps } from '../TestSuiteTree';
+import ThemeProvider from 'components/ThemeProvider';
 
 const runTestsMock = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -94,7 +95,11 @@ const testSuiteTreeProps: TestSuiteTreeProps = {
 };
 
 test('Test tree renders', () => {
-  render(<TestSuiteTree {...testSuiteTreeProps}></TestSuiteTree>);
+  render(
+    <ThemeProvider>
+      <TestSuiteTree {...testSuiteTreeProps} />
+    </ThemeProvider>
+  );
   const treeTitle = screen.getByText(testSuiteTreeProps.testSuite.title);
   expect(treeTitle).toBeVisible();
   const sequence1Title = screen.getByText(sequence1.title);
@@ -108,7 +113,11 @@ test('Test tree renders', () => {
 });
 
 test('Individual tests are not shown by default', () => {
-  render(<TestSuiteTree {...testSuiteTreeProps}></TestSuiteTree>);
+  render(
+    <ThemeProvider>
+      <TestSuiteTree {...testSuiteTreeProps} />
+    </ThemeProvider>
+  );
   sequence1.tests.forEach((test) => {
     const testTitle = screen.queryByText(test.title);
     expect(testTitle).toBeNull();
@@ -116,13 +125,21 @@ test('Individual tests are not shown by default', () => {
 });
 
 test('Calls setSelectedRunnable when tree item is clicked', () => {
-  render(<TestSuiteTree {...testSuiteTreeProps}></TestSuiteTree>);
+  render(
+    <ThemeProvider>
+      <TestSuiteTree {...testSuiteTreeProps} />
+    </ThemeProvider>
+  );
   const testSuiteLabel = screen.getByTestId(`tiLabel-${testSuiteTreeProps.testSuite.id}`);
   fireEvent.click(testSuiteLabel);
 });
 
 test('Calls runTests when run button is clicked', () => {
-  render(<TestSuiteTree {...testSuiteTreeProps}></TestSuiteTree>);
+  render(
+    <ThemeProvider>
+      <TestSuiteTree {...testSuiteTreeProps} />
+    </ThemeProvider>
+  );
   const suiteRunButton = screen.getByTestId(`runButton-${testSuiteTreeProps.testSuite.id}`);
   fireEvent.click(suiteRunButton);
   expect(runTestsMock).toHaveBeenCalledWith(
