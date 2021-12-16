726c7ac10a0dffbc50d805ef6544342354be94fa
Rob Scanlon <robscanlon@gmail.com>
2021-12-16 14:52:58 -0500

    Add modal when a test in a wait state. (#69)
    * Add modal when a test in a wait state.
    
    * Fix rubocop.
    
    * Ensure action modal does not open on undefined.
    
    * Update client/src/components/ActionModal/ActionModal.tsx
    
    Co-authored-by: Alyssa Wang <alyssa.c.wang@gmail.com>
    
    * Update client/src/components/ActionModal/ActionModal.tsx
    
    Co-authored-by: Alyssa Wang <alyssa.c.wang@gmail.com>
    
    * Clean up jsx.
    
    Co-authored-by: Alyssa Wang <alyssa.c.wang@gmail.com>

---
+++ client/src/components/ActionModal/ActionModal.tsx
@@ -0,0 +1,36 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+import React, { FC } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+export interface ActionModalProps {
+  modalVisible: boolean;
+  message?: string;
+  cancelTest: () => void;
+}
+
+const ActionModal: FC<ActionModalProps> = ({ modalVisible, message, cancelTest }) => {
+  return (
+    <Dialog open={modalVisible} fullWidth maxWidth="sm">
+      <DialogTitle>User Action Required</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          <ReactMarkdown>{message || ''}</ReactMarkdown>
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button color="primary" onClick={cancelTest} data-testid="cancel-button">
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ActionModal;
--- dev_suites/dev_demo_ig_stu1/demo_suite.rb
+++ dev_suites/dev_demo_ig_stu1/demo_suite.rb
@@ -28,6 +28,10 @@ module DemoIG_STU1 # rubocop:disable Naming/ClassAndModuleCamelCase
       exclude_message { |message| message.type == 'info' }
     end
 
+    config options: {
+      wait_test_url: "#{Inferno::Application['inferno_host']}/custom/demo/resume"
+    }
+
     group :simple_group do
       title 'Group 1'
       group from: 'DemoIG_STU1::DemoGroup', title: 'Demo Group Instance 1'
@@ -62,7 +66,15 @@ module DemoIG_STU1 # rubocop:disable Naming/ClassAndModuleCamelCase
         title 'Wait test'
         receives_request :resume
 
-        run { wait(identifier: 'abc') }
+        run do
+          wait(
+            identifier: 'abc',
+            message: %(
+              [Follow this link to proceed](#{config.options[:wait_test_url]}?xyz=abc).
+              Waiting to receive a request at ```#{config.options[:wait_test_url]}?xyz=abc```.
+            )
+          )
+        end
       end
 
       test do
