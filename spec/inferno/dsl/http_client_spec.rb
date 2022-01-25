d108c63e2b69def9cf1869560a066cbd5b8837c2
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-01-25 10:43:16 -0500

    Delete and Stream Support (#67)
    * assert_status_bad_or_unauthorized.
    
    * fhir_delete and unit tests.
    
    * stream and delete added to http_client with unit tests.
    
    * rubocop
    
    * Unit tests for http_client.
    
    * Linted and resultant changes.
    
    * minor param change, forgot to commit.
    
    * fhir_client changes and removed assertion.
    
    * fhir_client id change.
    
    * Removed default_setup_client
    
    * limit amount of response stored.

--- spec/inferno/dsl/http_client_spec.rb
+++ spec/inferno/dsl/http_client_spec.rb
@@ -323,6 +323,304 @@ RSpec.describe Inferno::DSL::HTTPClient do
     end
   end
 
+  describe '#stream' do
+    let(:generic_block) { proc { |chunk| chunk } }
+    let(:streamed) { [] }
+    let(:block) do
+      proc { |chunk|
+        streamed << chunk
+      }
+    end
+
+    context 'with a default client defined' do
+      before do
+        setup_default_client
+      end
+
+      context 'without a url argument' do
+        let(:stub_get_request) do
+          stub_request(:get, base_url)
+            .to_return(status: 200, body: response_body)
+        end
+
+        before { stub_get_request }
+
+        it "performs a HTTP GET to the default client's base url" do
+          group.stream(generic_block)
+
+          expect(stub_get_request).to have_been_made.once
+        end
+
+        it 'receives a chunk of the response via :block' do
+          group.stream(block)
+
+          expect(streamed).to eq([response_body])
+        end
+
+        it 'returns an Inferno::Entities::Request' do
+          result = group.stream(generic_block)
+
+          expect(result).to be_a(Inferno::Entities::Request)
+        end
+
+        it 'adds the request to the list of requests' do
+          result = group.stream(generic_block)
+
+          expect(group.requests).to include(result)
+          expect(group.request).to eq(result)
+        end
+
+        it 'stores the streamed chunk in the request response body' do
+          result = group.stream(generic_block)
+
+          expect(group.requests).to include(result)
+          expect(group.request.response_body).to eq(response_body)
+        end
+      end
+
+      context 'with a url argument' do
+        it 'performs a GET to the base_url + path' do
+          path = 'abc'
+          stubbed_request =
+            stub_request(:get, "#{base_url}/#{path}")
+              .to_return(status: 200, body: response_body, headers: {})
+
+          group.stream(generic_block, path)
+
+          expect(stubbed_request).to have_been_made.once
+        end
+      end
+
+      context 'with custom headers' do
+        it "performs a HTTP GET to the default client's base url" do
+          stub_get_header_request =
+            stub_request(:get, base_url)
+              .with(headers: { 'Warning' => 'Placeholder warning' })
+              .to_return(status: 200, body: '', headers: {})
+
+          group.stream(generic_block, headers: { 'Warning' => 'Placeholder warning' })
+          expect(stub_get_header_request).to have_been_made.once
+        end
+
+        it "perfoms a HTTP GET that includes the default client's existing headers" do
+          stub_get_header_request =
+            stub_request(:get, base_url)
+              .with(headers: { 'Clientheader' => 'DefaultHeader', 'Customheader' => 'MergedCustom' })
+              .to_return(status: 200, body: '', headers: {})
+
+          group.http_clients[:client_with_header] = client_with_header
+          group.stream(generic_block, client: :client_with_header, headers: { 'CustomHeader' => 'MergedCustom' })
+
+          expect(stub_get_header_request).to have_been_made.once
+        end
+      end
+
+      context 'with the client parameter' do
+        it 'uses that client' do
+          stub_get_request =
+            stub_request(:get, other_url)
+              .to_return(status: 200, body: '', headers: {})
+          group.http_clients[:other_client] = other_client
+
+          group.stream(generic_block, client: :other_client)
+
+          expect(stub_get_request).to have_been_made.once
+        end
+
+        it 'uses that client and its headers' do
+          stub_get_header_request =
+            stub_request(:get, base_url)
+              .with(headers: { 'ClientHeader' => 'DefaultHeader' })
+              .to_return(status: 200, body: '', headers: {})
+
+          group.http_clients[:client_with_header] = client_with_header
+          group.stream(generic_block, client: :client_with_header)
+
+          expect(stub_get_header_request).to have_been_made.once
+        end
+      end
+    end
+
+    context 'without a default client defined' do
+      it 'makes a request to an absolute url' do
+        url = 'https://example.com/abc'
+        stubbed_request =
+          stub_request(:get, url)
+            .to_return(status: 200, body: response_body)
+
+        group.stream(generic_block, url)
+
+        expect(stubbed_request).to have_been_made.once
+      end
+
+      it 'receives and stores a chunk of the response via :block' do
+        url = 'https://example.com/abc'
+        stub_request(:get, url)
+          .to_return(status: 200, body: response_body)
+
+        group.stream(block, url)
+
+        expect(streamed).to eq([response_body])
+      end
+
+      it 'raises an error if given a relative url' do
+        url = 'abc'
+
+        expect { group.stream(generic_block, url) }.to raise_error(/absolute url/)
+      end
+
+      it 'makes a request to an asbolute url with custom headers' do
+        url = 'https://example.com/abc'
+        stub_get_header_request =
+          stub_request(:get, url)
+            .with(headers: { 'Warning' => 'Placeholder warning' })
+            .to_return(status: 200, body: '', headers: {})
+
+        group.stream(generic_block, url, headers: { 'Warning' => 'Placeholder warning' })
+        expect(stub_get_header_request).to have_been_made.once
+      end
+
+      it 'stores the streamed chunk in the request response body' do
+        url = 'https://example.com/abc'
+        stub_request(:get, url)
+          .to_return(status: 200, body: response_body)
+
+        result = group.stream(block, url)
+
+        expect(group.requests).to include(result)
+        expect(group.request.response_body).to eq(response_body)
+      end
+    end
+  end
+
+  describe '#delete' do
+    context 'with a default client defined' do
+      before do
+        setup_default_client
+      end
+
+      context 'without a url argument' do
+        let(:stub_delete_request) do
+          stub_request(:delete, base_url)
+            .to_return(status: 202)
+        end
+
+        before { stub_delete_request }
+
+        it "performs a HTTP DELETE to the default client's base url" do
+          group.delete
+
+          expect(stub_delete_request).to have_been_made.once
+        end
+
+        it 'returns an Inferno::Entities::Request' do
+          result = group.delete
+
+          expect(result).to be_a(Inferno::Entities::Request)
+        end
+
+        it 'adds the request to the list of requests' do
+          result = group.delete
+
+          expect(group.requests).to include(result)
+          expect(group.request).to eq(result)
+        end
+      end
+
+      context 'with a url argument' do
+        it 'performs a DELETE to the base_url + path' do
+          path = 'abc'
+          stubbed_request =
+            stub_request(:delete, "#{base_url}/#{path}")
+              .to_return(status: 202)
+
+          group.delete(path)
+
+          expect(stubbed_request).to have_been_made.once
+        end
+      end
+
+      context 'with custom headers' do
+        it "performs a HTTP Delete to the default client's base url using existing headers" do
+          stub_delete_header_request =
+            stub_request(:delete, base_url)
+              .with(headers: { 'Warning' => 'Placeholder warning' })
+              .to_return(status: 202)
+
+          group.delete(headers: { 'Warning' => 'Placeholder warning' })
+          expect(stub_delete_header_request).to have_been_made.once
+        end
+
+        it "performs a HTTP DELETE to the default client's base url using both default and custom headers" do
+          stub_delete_header_request =
+            stub_request(:delete, base_url)
+              .with(headers: { 'Clientheader' => 'DefaultHeader', 'Customheader' => 'MergedCustom' })
+              .to_return(status: 202)
+
+          group.http_clients[:client_with_header] = client_with_header
+          group.delete(client: :client_with_header, headers: { 'CustomHeader' => 'MergedCustom' })
+
+          expect(stub_delete_header_request).to have_been_made.once
+        end
+      end
+
+      context 'with the client parameter' do
+        it 'uses that client' do
+          stub_delete_request =
+            stub_request(:delete, other_url)
+              .to_return(status: 202)
+          group.http_clients[:other_client] = other_client
+
+          group.delete(client: :other_client)
+
+          expect(stub_delete_request).to have_been_made.once
+        end
+
+        it 'uses that client and its headers' do
+          stub_delete_header_request =
+            stub_request(:delete, base_url)
+              .with(headers: { 'ClientHeader' => 'DefaultHeader' })
+              .to_return(status: 202)
+
+          group.http_clients[:client_with_header] = client_with_header
+          group.delete(client: :client_with_header)
+
+          expect(stub_delete_header_request).to have_been_made.once
+        end
+      end
+    end
+
+    context 'without a default client defined' do
+      it 'makes a request to an absolute url' do
+        url = 'https://example.com/abc'
+        stubbed_request =
+          stub_request(:delete, url)
+            .to_return(status: 200)
+
+        group.delete(url)
+
+        expect(stubbed_request).to have_been_made.once
+      end
+
+      it 'raises an error if given a relative url' do
+        url = 'abc'
+
+        expect { group.delete(url) }.to raise_error(/absolute url/)
+      end
+
+      it 'makes a request to an absolute url with custom headers' do
+        url = 'https://example.com/abc'
+        stub_delete_header_request =
+          stub_request(:delete, url)
+            .with(headers: { 'Warning' => 'Placeholder warning' })
+            .to_return(status: 200)
+
+        group.delete(url, headers: { 'Warning' => 'Placeholder warning' })
+        expect(stub_delete_header_request).to have_been_made.once
+      end
+    end
+  end
+
   describe '#requests' do
     it 'returns an array of the requests made' do
       stub_request(:get, base_url)
