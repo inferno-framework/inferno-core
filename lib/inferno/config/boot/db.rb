43567aebaff481436b357a7c2932c0295e029c08
Stephen MacVicar <smacvicar@mitre.org>
2022-02-15 08:24:01 -0500

    fix linting error

--- lib/inferno/config/boot/db.rb
+++ lib/inferno/config/boot/db.rb
@@ -14,19 +14,19 @@ Inferno::Application.boot(:db) do
     connection_attempts_remaining = ENV.fetch('MAX_DB_CONNECTION_ATTEMPTS', '10').to_i
     connection_retry_delay = ENV.fetch('DB_CONNECTION_RETRY_DELAY', '5').to_i
     connection = nil
-      loop do
-        connection = Sequel.connect(config)
-        break
-      rescue StandardError => e
-        connection_attempts_remaining -= 1
-        if connection_attempts_remaining.positive?
-          Inferno::Application['logger'].error("Unable to connect to database: #{e.message}")
-          Inferno::Application['logger'].error("#{connection_attempts_remaining} connection attempts remaining.")
-          sleep connection_retry_delay
-          next
-        end
-        raise
+    loop do
+      connection = Sequel.connect(config)
+      break
+    rescue StandardError => e
+      connection_attempts_remaining -= 1
+      if connection_attempts_remaining.positive?
+        Inferno::Application['logger'].error("Unable to connect to database: #{e.message}")
+        Inferno::Application['logger'].error("#{connection_attempts_remaining} connection attempts remaining.")
+        sleep connection_retry_delay
+        next
       end
+      raise
+    end
     connection.sql_log_level = :debug
 
     register('db.config', config)
