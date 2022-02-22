62442711f9df92cad8d3ce1375cae58fe497d92c
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-02-22 12:50:48 -0500

    update VersionsApi.ts

--- client/src/api/VersionsApi.ts
+++ client/src/api/VersionsApi.ts
@@ -1,15 +1,12 @@
 import { getApiEndpoint } from './infernoApiService';
 
 export function getCoreVersion(): Promise<string> {
-  let version = '';
   const endpoint = getApiEndpoint('/version');
   return fetch(endpoint)
     .then((response) => response.json())
     .then((result) => {
-      version = result.version as string;
-      return version;
-      // return 'version' in result ? (result.version as string) : '';
-    })
+      return 'version' in result ? (result.version as string) : '';
+    }) 
     .catch((e) => {
       console.log(e);
       return '';
