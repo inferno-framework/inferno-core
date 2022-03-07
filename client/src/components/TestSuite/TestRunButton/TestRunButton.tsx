47f0077e66648be129d3ccf851d9f84ec725d5de
Alyssa Wang <awang@mitre.org>
2022-03-07 16:53:50 -0500

    fix linting

--- client/src/components/TestSuite/TestRunButton/TestRunButton.tsx
+++ client/src/components/TestSuite/TestRunButton/TestRunButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC } from 'react';
 import { Button, Tooltip, IconButton } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
@@ -26,45 +26,48 @@ const TestRunButton: FC<TestRunButtonProps> = ({
   buttonText,
 }) => {
   const showRunButton = runnableIsTestSuite(runnable) || (runnable as TestGroup).user_runnable;
-  const runButton = showRunButton && (
-    <Tooltip title={testRunInProgress ? 'Disabled - Ongoing Test' : 'Run Test'} arrow>
-      {/* Necessary to enable tooltip on disabled button */}
-      <span>
-        {buttonText ? (
-          <Button
-            variant="contained"
-            disabled={testRunInProgress}
-            color="secondary"
-            size="small"
-            disableElevation
-            onClick={() => {
-              runTests(runnableType, runnable.id);
-            }}
-            endIcon={<PlayArrowIcon />}
-            data-testid={`runButton-${runnable.id}`}
-          >
-            {buttonText}
-          </Button>
-        ) : (
-          <IconButton
-            disabled={testRunInProgress}
-            color="secondary"
-            edge="end"
-            size="small"
-            onClick={() => {
-              runTests(runnableType, runnable.id);
-            }}
-            data-testid={`runButton-${runnable.id}`}
-            sx={{ margin: '0 4px' }}
-          >
-            <PlayCircleIcon />
-          </IconButton>
-        )}
-      </span>
-    </Tooltip>
-  );
 
-  return runButton;
+  return (
+    <>
+      {showRunButton && (
+        <Tooltip title={testRunInProgress ? 'Disabled - Ongoing Test' : 'Run Test'} arrow>
+          {/* Necessary to enable tooltip on disabled button */}
+          <span>
+            {buttonText ? (
+              <Button
+                variant="contained"
+                disabled={testRunInProgress}
+                color="secondary"
+                size="small"
+                disableElevation
+                onClick={() => {
+                  runTests(runnableType, runnable.id);
+                }}
+                endIcon={<PlayArrowIcon />}
+                data-testid={`runButton-${runnable.id}`}
+              >
+                {buttonText}
+              </Button>
+            ) : (
+              <IconButton
+                disabled={testRunInProgress}
+                color="secondary"
+                edge="end"
+                size="small"
+                onClick={() => {
+                  runTests(runnableType, runnable.id);
+                }}
+                data-testid={`runButton-${runnable.id}`}
+                sx={{ margin: '0 4px' }}
+              >
+                <PlayCircleIcon />
+              </IconButton>
+            )}
+          </span>
+        </Tooltip>
+      )}
+    </>
+  );
 };
 
 export default TestRunButton;
