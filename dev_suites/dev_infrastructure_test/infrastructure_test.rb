3c11abc720176b77fb0822cd85bd2fd2c184ae97
Rob Scanlon <robscanlon@gmail.com>
2022-03-11 13:43:04 -0500

    Require file to ensure it is loaded prior to test run.

--- dev_suites/dev_infrastructure_test/infrastructure_test.rb
+++ dev_suites/dev_infrastructure_test/infrastructure_test.rb
@@ -1,6 +1,7 @@
 require_relative 'external_outer_group'
 require_relative 'passing_optional_group'
 require_relative 'mixed_optional_group'
+require_relative 'failing_optional_group'
 
 module InfrastructureTest
   class Suite < Inferno::TestSuite
