99335cc183584ec2bc5f12ad5c62766783dde22a
Alyssa Wang <awang@mitre.org>
2022-03-17 12:10:05 -0400

    remove tooltip

--- client/src/components/TestSuite/TestRunButton/TestRunButton.tsx
+++ client/src/components/TestSuite/TestRunButton/TestRunButton.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Button, Tooltip, IconButton } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import { TestGroup, RunnableType, TestSuite, Test } from 'models/testSuiteModels';
@@ -25,43 +25,37 @@ const TestRunButton: FC<TestRunButtonProps> = ({
 
   return (
     <>
-      {showRunButton && (
-        <Tooltip title={testRunInProgress ? 'Disabled - Ongoing Test' : 'Run Test'} arrow>
-          {/* Necessary to enable tooltip on disabled button */}
-          <span>
-            {buttonText ? (
-              <Button
-                variant="contained"
-                disabled={testRunInProgress}
-                color="secondary"
-                size="small"
-                disableElevation
-                onClick={() => {
-                  runTests(runnableType, runnable.id);
-                }}
-                endIcon={<PlayArrowIcon />}
-                data-testid={`runButton-${runnable.id}`}
-              >
-                {buttonText}
-              </Button>
-            ) : (
-              <IconButton
-                disabled={testRunInProgress}
-                color="secondary"
-                edge="end"
-                size="small"
-                onClick={() => {
-                  runTests(runnableType, runnable.id);
-                }}
-                data-testid={`runButton-${runnable.id}`}
-                sx={{ margin: '0 4px' }}
-              >
-                <PlayCircleIcon />
-              </IconButton>
-            )}
-          </span>
-        </Tooltip>
-      )}
+      {showRunButton &&
+        (buttonText ? (
+          <Button
+            variant="contained"
+            disabled={testRunInProgress}
+            color="secondary"
+            size="small"
+            disableElevation
+            onClick={() => {
+              runTests(runnableType, runnable.id);
+            }}
+            endIcon={<PlayArrowIcon />}
+            data-testid={`runButton-${runnable.id}`}
+          >
+            {buttonText}
+          </Button>
+        ) : (
+          <IconButton
+            disabled={testRunInProgress}
+            color="secondary"
+            edge="end"
+            size="small"
+            onClick={() => {
+              runTests(runnableType, runnable.id);
+            }}
+            data-testid={`runButton-${runnable.id}`}
+            sx={{ margin: '0 4px' }}
+          >
+            <PlayCircleIcon />
+          </IconButton>
+        ))}
     </>
   );
 };
