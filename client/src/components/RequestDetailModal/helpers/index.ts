be8b4a5b2f54d14b1d456a7120a6a9a353e890ef
Chris Dillon <45436454+chris-at-work@users.noreply.github.com>
2022-04-25 13:57:50 -0700

    Format JSON requests as pretty printed documents (#177)
    * inferno-program did this formatting to make it readable rather than a single line of JSON
    * JSON parsing and pretty printing based on the response headers
    * leaving HTML alone, HTML likely has whitespaces already
    * added tests
    * some good manual tests are in the demo suite:  3.1.18 is HTML, 3.1.16 is JSON

---
+++ client/src/components/RequestDetailModal/helpers/index.ts
@@ -0,0 +1,32 @@
+import { RequestHeader } from 'models/testSuiteModels';
+
+export const formatBodyIfJSON = (
+  code: string,
+  headers: RequestHeader[] | null | undefined
+): string => {
+  // if we don't have metadata then do nothing
+  if (!headers) {
+    return code;
+  }
+
+  const contentTypeHeader = headers.find((h) => h.name === 'content-type');
+
+  let isJSON = false;
+  if (contentTypeHeader) {
+    const contentType = contentTypeHeader.value;
+    if (contentType.includes('application/fhir+json') || contentType.includes('application/json')) {
+      isJSON = true;
+    }
+  }
+
+  if (isJSON) {
+    return formatJSON(code);
+  } else {
+    // it is probably HTML so don't JSON format it
+    return code;
+  }
+};
+
+const formatJSON = (json: string): string => {
+  return JSON.stringify(JSON.parse(json), null, 2);
+};
