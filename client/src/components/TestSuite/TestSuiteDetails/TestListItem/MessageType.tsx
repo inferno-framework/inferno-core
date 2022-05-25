bcd389227b2bd5eb9ff7ff36232ff2a37b3c07d6
Chris Dillon <45436454+chris-at-work@users.noreply.github.com>
2022-05-25 09:04:40 -0700

    Change TestListItem error and warning messages to be more readable (#192)
    * show errors then warnings then info messages as a priority hierarchy
    * add colors and icons for readability
    * groups and sort messages by type
    * extract out a "ProblemBadge" component to represent a more focused badge

---
+++ client/src/components/TestSuite/TestSuiteDetails/TestListItem/MessageType.tsx
@@ -0,0 +1,37 @@
+import { Typography } from '@mui/material';
+import React, { FC } from 'react';
+import clsx from 'clsx';
+import useStyles from './styles';
+
+import Cancel from '@mui/icons-material/Cancel';
+import Warning from '@mui/icons-material/Warning';
+import Info from '@mui/icons-material/Info';
+
+type MessageTypeProps = {
+  type: 'warning' | 'error' | 'info';
+};
+
+const MessageType: FC<MessageTypeProps> = ({ type }) => {
+  const styles = useStyles();
+  const style = styles[type];
+
+  let icon;
+  if (type === 'error') {
+    icon = <Cancel className={style} />;
+  } else if (type === 'warning') {
+    icon = <Warning className={style} />;
+  } else if (type === 'info') {
+    icon = <Info className={style} />;
+  }
+
+  return (
+    <span>
+      {icon}
+      <Typography variant="caption" className={clsx([styles.messageTypeText, style])}>
+        {type}
+      </Typography>
+    </span>
+  );
+};
+
+export default MessageType;
