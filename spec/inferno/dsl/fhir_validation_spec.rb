7c4e666de90a3a353a20938042921a90276714f9
Jack Fraser <37051655+Jack-Fraser16@users.noreply.github.com>
2022-05-28 13:33:22 -0400

    reorganize and define fhir_validation tests

--- spec/inferno/dsl/fhir_validation_spec.rb
+++ spec/inferno/dsl/fhir_validation_spec.rb
@@ -74,9 +74,10 @@ RSpec.describe Inferno::DSL::FHIRValidation do
   end
 
   describe '#resource_is_valid?' do
-    it 'posts the resource with primitive extensions intact' do
-      resource_string = {
+    let(:resource_string) {
+      {
         resourceType: 'Patient',
+        id: '0000',
         _gender: {
           extension: [
             {
@@ -86,21 +87,55 @@ RSpec.describe Inferno::DSL::FHIRValidation do
           ]
         }
       }.to_json
-      resource = FHIR.from_contents(resource_string)
+    }
+    let(:resource) { FHIR.from_contents(resource_string) }
 
+    context 'with invalid resource' do
+      let(:invalid_outcome) {
+        {
+          resourceType: 'OperationOutcome',
+          issue: [
+            {
+              severity: "error",
+              code: "processing",
+              diagnostics: "Identifier.system must be an absolute reference, not a local reference",
+              location: [
+                  "Patient.identifier[0]",
+                  "Line 14, Col 10"
+              ]
+            }
+          ]
+        }.to_json
+      }
+
+      before do
+        stub_request(:post, "#{validation_url}/validate?profile=#{profile_url}") 
+          .with(body: resource_string)
+          .to_return(status: 200, body: invalid_outcome)
+      end 
+
+      it 'includes resourceType/id in error message' do
+        result = validator.resource_is_valid?(resource, profile_url, runnable)
+
+        expect(result).to eq(false)
+        expect(runnable.messages.first[:message]).to include("#{resource.resourceType}/#{resource.id}/")
+      end 
+
+      it 'includes resourceType/id in error message if resource.id is nil' do
+        resource.id = nil
+        result = validator.resource_is_valid?(resource, profile_url, runnable)
+        
+        expect(result).to eq(false)
+        expect(runnable.messages.first[:message]).to include("#{resource.resourceType}/")
+      end 
+    end 
+
+    it 'posts the resource with primitive extensions intact' do
       stub_request(:post, "#{validation_url}/validate?profile=#{profile_url}")
         .with(body: resource_string)
         .to_return(status: 200, body: FHIR::OperationOutcome.new.to_json)
 
       expect(validator.resource_is_valid?(resource, profile_url, runnable)).to be(true)
     end
-
-    it 'includes Resource/id in error message' do
-
-    end 
-
-    it 'includes Resource in error message if resource.id is nil' do
-
-    end 
   end
 end
