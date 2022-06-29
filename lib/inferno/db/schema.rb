b4e0dab29ad749a695fdd1bd73d081c222270810
Alyssa Wang <awang@mitre.org>
2022-06-29 12:36:18 -0400

    revert bundle changes

--- lib/inferno/db/schema.rb
+++ lib/inferno/db/schema.rb
@@ -5,7 +5,7 @@ Sequel.migration do
     end
     
     create_table(:test_sessions) do
-      String :id, :size=>255, :null=>false
+      String :id, :size=>36, :null=>false
       String :test_suite_id, :size=>255, :null=>false
       DateTime :created_at, :null=>false
       DateTime :updated_at, :null=>false
@@ -15,7 +15,7 @@ Sequel.migration do
     
     create_table(:session_data, :ignore_index_errors=>true) do
       String :id, :size=>255, :null=>false
-      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>255, :null=>false
+      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>36, :null=>false, :key=>[:id]
       String :name, :size=>255, :null=>false
       String :value, :text=>true
       
@@ -25,9 +25,9 @@ Sequel.migration do
     end
     
     create_table(:test_runs, :ignore_index_errors=>true) do
-      String :id, :size=>255, :null=>false
+      String :id, :size=>36, :null=>false
       String :status, :size=>255
-      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>255
+      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>36, :key=>[:id]
       String :test_suite_id, :size=>255
       String :test_group_id, :size=>255
       String :test_id, :size=>255
@@ -47,11 +47,11 @@ Sequel.migration do
     end
     
     create_table(:results, :ignore_index_errors=>true) do
-      String :id, :size=>255, :null=>false
-      foreign_key :test_run_id, :test_runs, :type=>String, :size=>255, :null=>false
-      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>255, :null=>false
+      String :id, :size=>36, :null=>false
+      foreign_key :test_run_id, :test_runs, :type=>String, :size=>36, :null=>false, :key=>[:id]
+      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>36, :null=>false, :key=>[:id]
       String :result, :size=>255
-      String :result_message, :size=>255
+      String :result_message, :text=>true
       String :test_suite_id, :size=>255
       String :test_group_id, :size=>255
       String :test_id, :size=>255
@@ -72,10 +72,10 @@ Sequel.migration do
     
     create_table(:messages, :ignore_index_errors=>true) do
       primary_key :index
-      String :id, :size=>255, :null=>false
-      foreign_key :result_id, :results, :type=>String, :size=>255, :null=>false
+      String :id, :size=>36, :null=>false
+      foreign_key :result_id, :results, :type=>String, :size=>36, :null=>false, :key=>[:id]
       String :type, :size=>255, :null=>false
-      String :message, :size=>255, :null=>false
+      String :message, :text=>true, :null=>false
       DateTime :created_at, :null=>false
       DateTime :updated_at, :null=>false
       
@@ -85,31 +85,31 @@ Sequel.migration do
     
     create_table(:requests, :ignore_index_errors=>true) do
       primary_key :index
-      String :id, :size=>255, :null=>false
+      String :id, :size=>36, :null=>false
       String :verb, :size=>255, :null=>false
-      String :url, :size=>255, :null=>false
+      String :url, :text=>true, :null=>false
       String :direction, :size=>255, :null=>false
       Integer :status
       String :name, :size=>255
       String :request_body, :text=>true
       String :response_body, :text=>true
-      foreign_key :result_id, :results, :type=>String, :size=>255, :null=>false
-      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>255, :null=>false
-      String :"[:test_session_id, :name]"
+      foreign_key :result_id, :results, :type=>String, :size=>36, :null=>false, :key=>[:id]
+      foreign_key :test_session_id, :test_sessions, :type=>String, :size=>36, :null=>false, :key=>[:id]
       DateTime :created_at, :null=>false
       DateTime :updated_at, :null=>false
       
-      index [:id]
+      index [:id], :unique=>true
       index [:result_id]
       index [:test_session_id]
+      index [:test_session_id, :name]
     end
     
     create_table(:headers, :ignore_index_errors=>true) do
-      String :id, :size=>255, :null=>false
-      foreign_key :request_id, :requests, :type=>String, :size=>255, :null=>false
+      String :id, :size=>36, :null=>false
+      foreign_key :request_id, :requests, :null=>false, :key=>[:index]
       String :type, :size=>255, :null=>false
       String :name, :size=>255, :null=>false
-      String :value, :size=>255
+      String :value, :text=>true
       DateTime :created_at, :null=>false
       DateTime :updated_at, :null=>false
       
@@ -118,8 +118,8 @@ Sequel.migration do
     end
     
     create_table(:requests_results, :ignore_index_errors=>true) do
-      foreign_key :results_id, :results, :type=>String, :size=>255, :null=>false
-      foreign_key :requests_id, :requests, :type=>String, :size=>255, :null=>false
+      foreign_key :results_id, :results, :type=>String, :size=>36, :null=>false, :key=>[:id]
+      foreign_key :requests_id, :requests, :null=>false, :key=>[:index]
       
       index [:requests_id]
       index [:results_id]
