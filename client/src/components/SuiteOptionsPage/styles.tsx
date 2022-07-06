09c05b177206c7ed8a57ce96261173fd41c8c6d7
Rob Scanlon <robscanlon@gmail.com>
2022-07-06 12:18:32 -0400

    FI-1608-options-ui Initial functional options UI (#215)
    * Initial draft of suite options ui
    
    * Fix lint issues.
    
    * include params in body in session creation controller
    
    Co-authored-by: Stephen MacVicar <smacvicar@mitre.org>

---
+++ client/src/components/SuiteOptionsPage/styles.tsx
@@ -0,0 +1,26 @@
+import { Theme } from '@mui/material/styles';
+
+import makeStyles from '@mui/styles/makeStyles';
+
+export default makeStyles((theme: Theme) => ({
+  container: {
+    backgroundColor: theme.palette.common.white,
+  },
+  main: {
+    height: '100%',
+    display: 'flex',
+    padding: '0 60px',
+  },
+  selectedItem: {
+    backgroundColor: 'rgba(248, 139, 48, 0.2) !important',
+  },
+  startTesting: {
+    marginTop: '20px',
+    padding: '20px',
+    borderRadius: '16px',
+    width: '400px',
+  },
+  startTestingButton: {
+    fontWeight: 600,
+  },
+}));
