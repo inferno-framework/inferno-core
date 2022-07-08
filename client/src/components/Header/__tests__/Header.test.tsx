5b02303fa611306179d8083e206ee223276afa79
Chris Dillon <cdillon@mitre.org>
2022-07-08 13:34:15 -0700

    Enable windowIsSmall test by configuring zustand testing boilerplate

--- client/src/components/Header/__tests__/Header.test.tsx
+++ client/src/components/Header/__tests__/Header.test.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ThemeProvider from 'components/ThemeProvider';
 import Header from '../Header';
+
+// this testing helper is needed to test react hooks outside of render
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useAppStore } from '~/store/app';
+
+// boilerplate for mocking zustand which uses hooks outside of a component
+beforeEach(() => {
+  const { result } = renderHook(() => useAppStore((state) => state));
+
+  act(() => {
+    result.current.windowIsSmall = true;
+  });
+});
 
 test('renders wide screen Inferno Header', () => {
   let drawerOpen = true;
@@ -23,28 +37,27 @@ test('renders wide screen Inferno Header', () => {
   expect(titleElement).toHaveTextContent('Suite Title');
 });
 
-// Commenting out until window size/Zustand is mocked
-// test('renders narrow screen Inferno Header', () => {
-//   let drawerOpen = false;
+test('renders narrow screen Inferno Header', () => {
+  let drawerOpen = false;
 
-//   render(
-//     <ThemeProvider>
-//       <Header
-//         suiteTitle="Suite Title"
-//         drawerOpen={drawerOpen}
-//         toggleDrawer={() => (drawerOpen = !drawerOpen)}
-//       />
-//     </ThemeProvider>
-//   );
+  render(
+    <ThemeProvider>
+      <Header
+        suiteTitle="Suite Title"
+        drawerOpen={drawerOpen}
+        toggleDrawer={() => (drawerOpen = !drawerOpen)}
+      />
+    </ThemeProvider>
+  );
 
-//   const logoElement = screen.getByRole('img');
-//   expect(logoElement).toHaveAttribute('alt', 'Inferno logo');
+  const logoElement = screen.getByRole('img');
+  expect(logoElement).toHaveAttribute('alt', 'Inferno logo');
 
-//   // test icon drawer control
-//   expect(drawerOpen).toBe(false);
-//   userEvent.click(logoElement);
-//   expect(drawerOpen).toBe(true);
-// });
+  // test icon drawer control
+  expect(drawerOpen).toBe(false);
+  userEvent.click(logoElement);
+  expect(drawerOpen).toBe(true);
+});
 
 // Commenting out for now
 // We need to refine how the header works
