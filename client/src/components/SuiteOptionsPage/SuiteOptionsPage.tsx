35e1a033c463d8c64e50ac501222769912d9b94b
Alyssa Wang <alyssa.c.wang@gmail.com>
2022-07-27 10:58:47 -0400

    feature/FI-1614-options-clean (#222)
    * update suite options page to match landing page
    
    * remove extra components
    
    * simplify conditional
    
    * change == to ===
    
    * remove require rake
    
    Co-authored-by: Alyssa Wang <awang@mitre.org>

--- client/src/components/SuiteOptionsPage/SuiteOptionsPage.tsx
+++ client/src/components/SuiteOptionsPage/SuiteOptionsPage.tsx
@@ -7,28 +7,29 @@ import {
   Container,
   Button,
   Paper,
-  List,
-  Grid,
   Radio,
   RadioGroup,
+  Box,
 } from '@mui/material';
 import useStyles from './styles';
 import { useHistory, useParams } from 'react-router-dom';
 import { postTestSessions } from '~/api/TestSessionApi';
 import { TestSuite, TestSession, SuiteOption } from '~/models/testSuiteModels';
 import ReactMarkdown from 'react-markdown';
+import { useAppStore } from '~/store/app';
 
 export interface SuiteOptionsPageProps {
   testSuites: TestSuite[] | undefined;
 }
 
 const SuiteOptionsPage: FC<SuiteOptionsPageProps> = ({ testSuites }) => {
+  const windowIsSmall = useAppStore((state) => state.windowIsSmall);
   const styles = useStyles();
   const history = useHistory();
 
   const { test_suite_id } = useParams<{ test_suite_id: string }>();
 
-  const testSuite = testSuites?.find((suite: TestSuite) => suite.id == test_suite_id);
+  const testSuite = testSuites?.find((suite: TestSuite) => suite.id === test_suite_id);
 
   // just grab the first to start
   // perhaps choices should be persisted in the URL to make it easy to share specific
@@ -65,73 +66,63 @@ const SuiteOptionsPage: FC<SuiteOptionsPageProps> = ({ testSuites }) => {
 
   return (
     <Container maxWidth="lg" className={styles.main} role="main">
-      <Grid container spacing={6}>
-        <Grid container item xs={6}>
-          <Grid item>
-            <Typography variant="h2" component="h1">
-              {testSuite?.title}
-            </Typography>
-            <Typography variant="h5" component="h2">
-              <ReactMarkdown>{testSuite?.description || ''}</ReactMarkdown>
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container item xs={6}>
-          <Grid item>
-            <Paper elevation={4} className={styles.startTesting}>
-              <Typography variant="h4" component="h2" align="center">
-                Select Options
-              </Typography>
-              {testSuite?.suite_options?.map((suiteOption: SuiteOption, i) => (
-                <FormControl
-                  fullWidth
-                  id={`suite-option-input-${i}`}
-                  key={`suite-form-control${i}`}
-                >
-                  <FormLabel>{suiteOption.title}</FormLabel>
-                  <RadioGroup
-                    row
-                    aria-label={`suite-option-group-${suiteOption.id}`}
-                    defaultValue={
-                      suiteOption.list_options &&
-                      suiteOption.list_options.length &&
-                      suiteOption.list_options[0].value
-                    }
-                    name={`suite-option-group-${suiteOption.id}`}
-                  >
-                    {suiteOption &&
-                      suiteOption.list_options &&
-                      suiteOption.list_options.map((choice, k) => (
-                        <FormControlLabel
-                          value={choice.value}
-                          control={<Radio size="small" />}
-                          label={choice.label}
-                          key={`radio-button-${k}`}
-                          onClick={() => {
-                            changeSuiteOption(suiteOption.id, choice.value);
-                          }}
-                        />
-                      ))}
-                  </RadioGroup>
-                </FormControl>
-              ))}
-
-              <List></List>
-              <Button
-                variant="contained"
-                size="large"
-                color="primary"
-                fullWidth
-                data-testid="go-button"
-                className={styles.startTestingButton}
-                onClick={() => createTestSession()}
+      <Box display="flex" flexDirection="column" m={2} maxWidth="440px">
+        <Typography variant="h2" component="h1">
+          {testSuite?.title}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          <ReactMarkdown>{testSuite?.description || ''}</ReactMarkdown>
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" height="fit-content">
+        <Paper
+          elevation={4}
+          className={styles.startTesting}
+          sx={{ width: windowIsSmall ? 'auto' : '400px' }}
+        >
+          <Typography variant="h4" component="h2" align="center">
+            Select Options
+          </Typography>
+          {testSuite?.suite_options?.map((suiteOption: SuiteOption, i) => (
+            <FormControl fullWidth id={`suite-option-input-${i}`} key={`suite-form-control${i}`}>
+              <FormLabel>{suiteOption.title}</FormLabel>
+              <RadioGroup
+                row
+                aria-label={`suite-option-group-${suiteOption.id}`}
+                defaultValue={
+                  suiteOption.list_options &&
+                  suiteOption.list_options.length &&
+                  suiteOption.list_options[0].value
+                }
+                name={`suite-option-group-${suiteOption.id}`}
               >
-                Start Testing
-              </Button>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Grid>
+                {suiteOption?.list_options?.map((choice, k) => (
+                  <FormControlLabel
+                    value={choice.value}
+                    control={<Radio size="small" />}
+                    label={choice.label}
+                    key={`radio-button-${k}`}
+                    onClick={() => {
+                      changeSuiteOption(suiteOption.id, choice.value);
+                    }}
+                  />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          ))}
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            fullWidth
+            data-testid="go-button"
+            className={styles.startTestingButton}
+            onClick={() => createTestSession()}
+          >
+            Start Testing
+          </Button>
+        </Paper>
+      </Box>
     </Container>
   );
 };
