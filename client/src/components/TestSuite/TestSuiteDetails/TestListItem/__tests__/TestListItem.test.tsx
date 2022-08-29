db745498fb5fa0f7542b90595ab99297fa89b298
Chris Dillon <45436454+chris-at-work@users.noreply.github.com>
2022-08-29 10:38:45 -0700

    Fix markdown tables not rendering in messages (#248)
    Github Flavored Markdown (GFM) has a tables syntax.  GFM is not in the
    react-markdown library we are using.  Add the GFM plugin that makes table
    rendering possible.  In addition, the plugin did not work at first with
    the existing versions that were locked (react-markdown 6.0.2):
    
    ```
    Uncaught TypeError: self.parser.lazy is undefined
    ```
    
    So I bumped the versions to latest of react-markdown and the gfm plugin and
    then it worked.

---
+++ client/src/components/TestSuite/TestSuiteDetails/TestListItem/__tests__/TestListItem.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import ThemeProvider from 'components/ThemeProvider';
+
+import TestListItem from '../TestListItem';
+import { Test, TestInput, TestOutput } from '~/models/testSuiteModels';
+
+describe('The TestListItem component', () => {
+  test('it renders a table with the test description as markdown', () => {
+    // If you indent this with four spaces as whitespace indent,
+    // markdown will think this is <code>.  If javascript had
+    // heredocs then it might be able to strip whitespace.  So,
+    // just add newlines to the string.
+    const markDownTable = `| Head1 | Head2 |\n| --- | --- |\n| Row1 | Row2 |`;
+
+    const outputs: TestOutput[] = [{ name: 'One', value: 'one' }];
+    const inputs: TestInput[] = [{ name: 'two', value: 'two' }];
+    const test: Test = {
+      id: 'test_id',
+      title: 'Test Title',
+      inputs: inputs,
+      short_id: 'short_id',
+      outputs: outputs,
+      user_runnable: false,
+      description: markDownTable,
+    };
+
+    render(
+      <ThemeProvider>
+        <TestListItem test={test} testRunInProgress={false} view="run" />
+      </ThemeProvider>
+    );
+
+    const accordion = screen.getByTitle('test_id-summary');
+    fireEvent.click(accordion);
+
+    const aboutTab = screen.getByRole('tab', { name: 'About' });
+    fireEvent.click(aboutTab);
+
+    expect(screen.getByText('Head1').closest('thead')).toBeInTheDocument();
+    expect(screen.getByText('Row1').closest('tbody')).toBeInTheDocument();
+  });
+});
