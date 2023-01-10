bcd182007c6c0f239f54b311c2f709d9d5da4073
Stephen MacVicar <smacvicar@mitre.org>
2023-01-10 15:34:05 -0500

    add suite description cli command

--- lib/inferno/apps/cli/suite.rb
+++ lib/inferno/apps/cli/suite.rb
@@ -1,3 +1,5 @@
+require 'tty-markdown'
+
 require_relative 'suite_input_template'
 
 module Inferno
@@ -13,6 +15,40 @@ module Inferno
       option :filename, banner: '<filename>', aliases: [:f]
       def input_template(suite_id)
         SuiteInputTemplate.new.run(suite_id, options)
+      end
+
+      desc 'describe SUITE_ID', 'Show information about a suite'
+      long_desc <<~LONGDESC
+        Display a suite's description and available options.
+      LONGDESC
+      def describe(suite_id)
+        Inferno::Application.start(:suites)
+
+        suite = Inferno::Repositories::TestSuites.new.find(suite_id)
+
+        if suite.blank?
+          message = "No suite found with id `#{suite_id}`. Run `inferno suites` to see a list of available suites"
+
+          puts TTY::Markdown.parse(message)
+          return
+        end
+
+        description = ''
+        description += "# #{suite.title}\n"
+        description += suite.description
+        description += "\n***\n\n"
+
+        if suite.suite_options.present?
+          description += "# Suite Options\n\n"
+          suite.suite_options.each do |option|
+            description += "* `#{option.id}`: #{option.title}\n"
+            option.list_options.each do |list_option|
+              description += "  * `#{list_option[:value]}`: #{list_option[:label]}\n"
+            end
+          end
+        end
+
+        puts TTY::Markdown.parse(description)
       end
     end
   end
