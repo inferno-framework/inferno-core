a504c5abf8c879fea042fbed213e221f384ec714
Shaumik-Ashraf <shaumikashraf@mitre.org>
2023-11-10 15:38:00 -0500

    implementing erb templates

--- lib/inferno/apps/cli/new/new.rb
+++ lib/inferno/apps/cli/new/new.rb
@@ -3,7 +3,7 @@ require 'thor'
 
 module Inferno
   module CLI
-    class New < Thor
+    class New
       include Thor::Actions
 
       @@inflector = Dry::Inflector.new do |inflections|
@@ -19,7 +19,7 @@ module Inferno
 
         ## Template Generation:
         # copies all files from ./templates/ folder
-        # performs ERB substitution on all .tt files and remove .tt suffix
+        # performs ERB substitution on all .tt files and removes .tt suffix
         # replaces all %foo% file names with foo() method call
         directory('.', root_name)
         
@@ -30,14 +30,9 @@ module Inferno
         @@inflector.dasherize(@name)
       end
 
-      # lib folder name, i.e: inferno_template
+      # library name, i.e: inferno_template
       def lib_name
         @@inflector.underscore(@name)
-      end
-
-      # file name with suffix extension, i.e: inferno_template.rb
-      def file_name(suffix = '.rb')
-        @@inflector.underscore(@name) + suffix
       end
 
       # module name, i.e: InfernoTemplate
@@ -48,6 +43,16 @@ module Inferno
       # English grammatical name, i.e: Inferno template
       def human_name
         @@inflector.humanize(@name)
+      end
+
+      # title case name, i.e: Inferno Template
+      def title_name
+        human_name.split(' ').map{ |s| s.capitalize }.join(' ')
+      end
+
+      # suffix '_test_suite' in snake case, i.e: inferno_template_test_suite
+      def test_suite_id
+        "#{lib_name}_test_suite"
       end
     end
   end
