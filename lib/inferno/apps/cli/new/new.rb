e988af639b4459ea1fc1d86606db3fce0a320342
Shaumik-Ashraf <shaumikashraf@mitre.org>
2023-11-10 14:52:17 -0500

    use .tt suffix because it is built into thor

--- lib/inferno/apps/cli/new/new.rb
+++ lib/inferno/apps/cli/new/new.rb
@@ -17,37 +17,38 @@ module Inferno
       def run(name, implementation_guide = nil)
         @name = name
 
-        ## This defines the new inferno-template:
-        empty_directory(folder_name)
-        template(
-
+        ## Template Generation:
+        # copies all files from ./templates/ folder
+        # performs ERB substitution on all .tt files and remove .tt suffix
+        # replaces all %foo% file names with foo() method call
+        directory('.', root_name)
+        
       end
 
-      private
-        # root folder name, i.e: inferno-template
-        def folder_name
-          @@inflector.dasherize(@name)
-        end
+      # root folder name, i.e: inferno-template
+      def root_name
+        @@inflector.dasherize(@name)
+      end
 
-        # lib folder name, i.e: inferno_template
-        def lib_name
-          @@inflector.underscore(@name)
-        end
+      # lib folder name, i.e: inferno_template
+      def lib_name
+        @@inflector.underscore(@name)
+      end
 
-        # file name with suffix extension, i.e: inferno_template.rb
-        def file_name(suffix = '.rb')
-          @@inflector.underscore(@name) + suffix
-        end
+      # file name with suffix extension, i.e: inferno_template.rb
+      def file_name(suffix = '.rb')
+        @@inflector.underscore(@name) + suffix
+      end
 
-        # module name, i.e: InfernoTemplate
-        def module_name
-          @@inflector.camelize(@name)
-        end
+      # module name, i.e: InfernoTemplate
+      def module_name
+        @@inflector.camelize(@name)
+      end
 
-        # English grammatical name, i.e: Inferno template
-        def human_name
-          @@inflector.humanize(@name)
-        end
+      # English grammatical name, i.e: Inferno template
+      def human_name
+        @@inflector.humanize(@name)
+      end
     end
   end
 end
