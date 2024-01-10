1c4ae0bcf91de4a341741249dba13936ea72ecec
360dgries <dgriese@mitre.org>
2024-01-10 14:54:19 -0500

    Allow for form data at session create endpoint

--- lib/inferno/apps/web/controllers/test_sessions/create.rb
+++ lib/inferno/apps/web/controllers/test_sessions/create.rb
@@ -10,7 +10,9 @@ module Inferno
 
           def handle(req, res)
             params = req.params.to_h
-            params.merge!(JSON.parse(req.body.string).symbolize_keys) unless req.body.string.blank?
+            unless req.body.string.blank? || req.env['CONTENT_TYPE'].include?('multipart/form-data')
+              params.merge!(JSON.parse(req.body.string).symbolize_keys)
+            end
 
             session = repo.create(create_params(params))
 
