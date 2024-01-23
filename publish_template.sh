38df5c7528de46fdb976c5db4a16dff88a80d3a3
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-01-23 16:45:43 -0500

    test script

--- publish_template.sh
+++ publish_template.sh
@@ -8,18 +8,20 @@
 set -e
 set -x
 
+
+REPO_URL=${1:-git@github.com:inferno-framework/inferno-template.git}
+VERSION=$(bundle exec ./bin/inferno version)
+
+
 if [ -d ./inferno-template ]; then
   echo "ERROR: detected inferno-template directory, please remove or rename it to run this script."
   exit 1;
 fi;
 
-REPO_URL=git@github.com:inferno-framework/inferno-template.git
-VERSION="$(bundle exec ./bin/inferno version)"
-
 git clone $REPO_URL ./inferno-template
 rm -f ./inferno-template/Gemfile.lock
 
-bundle exec ./bin/inferno new inferno-template --author 'Inferno Template' --force
+bundle exec ./bin/inferno new inferno-template --author "Inferno Template" --force
 
 git -C ./inferno-template add --all
 git -C ./inferno-template commit --message "update inferno-template to v$VERSION"
