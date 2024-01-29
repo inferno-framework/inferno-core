b903cc39cbfcba669906257a235c155ce4efd0ac
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-01-29 13:53:13 -0500

    move and fix spec file

--- spec/inferno/apps/cli/main_spec.rb
+++ spec/inferno/apps/cli/main_spec.rb
@@ -6,7 +6,7 @@ require 'inferno/version'
 RSpec.describe Inferno::CLI::Main do # rubocop:disable RSpec/FilePath
   context 'with version command' do
     it 'outputs current Inferno version' do
-      expect { described_class.new.version }.to output("#{Inferno::VERSION}\n").to_stdout
+      expect { described_class.new.version }.to output("Inferno Core v#{Inferno::VERSION}\n").to_stdout
     end
   end
 end
