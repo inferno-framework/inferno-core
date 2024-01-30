6fe48ebef2bc54abe94b94bcb14df047218eac8d
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-01-30 11:56:11 -0500

    make script use tmp folder and push to branch

--- publish_template.sh
+++ publish_template.sh
@@ -11,20 +11,21 @@ set -x
 
 REPO_URL=${1:-git@github.com:inferno-framework/inferno-template.git}
 VERSION=$(bundle exec ./bin/inferno version)
+BRANCH=$(echo "Update $VERSION" | tr ' ' '-')
 
-
-if [ -d ./inferno-template ]; then
-  echo "ERROR: detected inferno-template directory, please remove or rename it to run this script."
-  exit 1;
-fi;
-
+cd ./tmp
 git clone $REPO_URL ./inferno-template
 rm -f ./inferno-template/Gemfile.lock
 
-bundle exec ./bin/inferno new inferno-template --author "Inferno Template" --force
+git -C ./inferno-template branch $BRANCH
+git -C ./inferno-template checkout $BRANCH
+
+bundle exec ../bin/inferno new inferno-template --author "Inferno Template" --force
 
 git -C ./inferno-template add --all
-git -C ./inferno-template commit --message "update inferno-template to v$VERSION"
-git -C ./inferno-template push origin main
+git -C ./inferno-template commit --message "Update to $VERSION"
+git -C ./inferno-template push --set-upstream origin $BRANCH
+cd ..
 
-rm -rf ./inferno-template
+rm -rf ./tmp/inferno-template
+echo "Pushed to branch $BRANCH, please put up a pull request and merge to main."
