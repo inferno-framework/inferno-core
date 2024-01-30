40621fb3a844ef6938941305be389a7b43447cc6
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-01-30 13:57:30 -0500

    clean script

--- publish_template.sh
+++ publish_template.sh
@@ -13,19 +13,20 @@ REPO_URL=${1:-git@github.com:inferno-framework/inferno-template.git}
 VERSION=$(bundle exec ./bin/inferno version)
 BRANCH=$(echo "Update $VERSION" | tr ' [:upper:]' '-[:lower:]')
 
+
 cd ./tmp
 git clone $REPO_URL ./inferno-template
-rm -f ./inferno-template/Gemfile.lock
-
 git -C ./inferno-template branch $BRANCH
 git -C ./inferno-template checkout $BRANCH
+
+rm -f ./inferno-template/Gemfile.lock
 
 bundle exec ../bin/inferno new inferno-template --author "Inferno Template" --force
 
 git -C ./inferno-template add --all
 git -C ./inferno-template commit --message "Update to $VERSION"
 git -C ./inferno-template push --set-upstream origin $BRANCH
+mv ./inferno-template "./inferno-template-$BRANCH"
 cd ..
 
-mv ./tmp/inferno-template "./tmp/inferno-template-$BRANCH"
 echo "Pushed to branch $BRANCH, please put up a pull request and merge to main."
