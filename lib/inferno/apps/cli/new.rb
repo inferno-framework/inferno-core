54e8a1ded55670306d7799fee7e4d62a8fa2e482
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-02-19 15:56:20 -0500

    remove redundant begin

--- lib/inferno/apps/cli/new.rb
+++ lib/inferno/apps/cli/new.rb
@@ -106,15 +106,13 @@ module Inferno
       def load_igs
         config = { verbose: !options['quiet'] }
         options['implementation_guide']&.each_with_index do |ig, idx|
-          begin # rubocop:disable Style/RedundantBegin
-            uri = options['implementation_guide'].length == 1 ? load_ig(ig, nil, config) : load_ig(ig, idx, config)
-            say_unless_quiet "Downloaded IG from #{uri}"
-          rescue OpenURI::HTTPError => e
-            say_unless_quiet "Failed to install implementation guide #{ig}", :red
-            say_unless_quiet e.message, :red
-          rescue StandardError => e
-            say_unless_quiet e.message, :red
-          end
+          uri = options['implementation_guide'].length == 1 ? load_ig(ig, nil, config) : load_ig(ig, idx, config)
+          say_unless_quiet "Downloaded IG from #{uri}"
+        rescue OpenURI::HTTPError => e
+          say_unless_quiet "Failed to install implementation guide #{ig}", :red
+          say_unless_quiet e.message, :red
+        rescue StandardError => e
+          say_unless_quiet e.message, :red
         end
       end
 
