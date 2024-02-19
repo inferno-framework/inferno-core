9add09d5b4ec8a502d7df8f5cbe87d487791ef09
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-02-19 15:29:51 -0500

    add describe blocks

--- spec/inferno/utils/ig_downloader_spec.rb
+++ spec/inferno/utils/ig_downloader_spec.rb
@@ -27,16 +27,20 @@ RSpec.describe Inferno::Utils::IgDownloader do
   end
   let(:package_binary) { File.read(PACKAGE_FIXTURE) }
 
-  it 'builds correct path to IGs' do
-    expect(ig_downloader.ig_path).to eq('lib/udap/igs')
+  describe '#ig_path' do
+    it 'builds correct path to IGs' do
+      expect(ig_downloader.ig_path).to eq('lib/udap/igs')
+    end
   end
 
-  it 'builds correct IG file path' do
-    expect(ig_downloader.ig_file).to eq('lib/udap/igs/package.tgz')
-  end
+  describe '#ig_file' do
+    it 'builds correct IG file path' do
+      expect(ig_downloader.ig_file).to eq('lib/udap/igs/package.tgz')
+    end
 
-  it 'suffixes IG file path' do
-    expect(ig_downloader.ig_file(99)).to eq('lib/udap/igs/package_99.tgz')
+    it 'suffixes IG file path' do
+      expect(ig_downloader.ig_file(99)).to eq('lib/udap/igs/package_99.tgz')
+    end
   end
 
   context 'with IG by canonical name' do
@@ -45,28 +49,34 @@ RSpec.describe Inferno::Utils::IgDownloader do
       'https://packages.simplifier.net/hl7.fhir.us.udap-security/-/hl7.fhir.us.udap-security-1.0.0.tgz'
     end
 
-    it 'matches fhir package name regex' do
-      expect(canonical).to match(Inferno::Utils::IgDownloader::FHIR_PACKAGE_NAME_REG_EX)
+    describe 'FHIR_PACKAGE_NAME_REG_EX' do
+      it 'matches given canonical name' do
+        expect(canonical).to match(Inferno::Utils::IgDownloader::FHIR_PACKAGE_NAME_REG_EX)
+      end
     end
 
-    it 'returns correct registry url' do
-      expect(ig_downloader.ig_registry_url(canonical)).to eq(resolved_url)
+    describe '#ig_registry_url' do
+      it 'returns correct registry url' do
+        expect(ig_downloader.ig_registry_url(canonical)).to eq(resolved_url)
+      end
+
+      it 'raises exception if missing version' do
+        expect do
+          ig_downloader.ig_registry_url('hl7.fhir.us.udap-security')
+        end.to raise_error(Inferno::Utils::IgDownloader::Error)
+      end
     end
 
-    it 'raises exception if missing version' do
-      expect do
-        ig_downloader.ig_registry_url('hl7.fhir.us.udap-security')
-      end.to raise_error(Inferno::Utils::IgDownloader::Error)
-    end
+    describe '#load_ig' do
+      it 'successfully downloads IG' do
+        stub_request(:get, 'https://packages.simplifier.net/hl7.fhir.us.udap-security/-/hl7.fhir.us.udap-security-1.0.0.tgz')
+          .to_return(body: package_binary)
 
-    it 'downloads IG' do
-      stub_request(:get, 'https://packages.simplifier.net/hl7.fhir.us.udap-security/-/hl7.fhir.us.udap-security-1.0.0.tgz')
-        .to_return(body: package_binary)
-
-      with_temp_path('ig-downloader-canonical') do |temp_path|
-        allow(ig_downloader).to receive(:ig_file).and_return(temp_path)
-        ig_downloader.load_ig(canonical, nil, { verbose: false })
-        expect(File.read(temp_path)).to eq(package_binary)
+        with_temp_path('ig-downloader-canonical') do |temp_path|
+          allow(ig_downloader).to receive(:ig_file).and_return(temp_path)
+          ig_downloader.load_ig(canonical, nil, { verbose: false })
+          expect(File.read(temp_path)).to eq(package_binary)
+        end
       end
     end
   end
@@ -79,24 +89,32 @@ RSpec.describe Inferno::Utils::IgDownloader do
     http://build.fhir.org/ig/HL7/fhir-udap-security-ig/
   ].each do |url|
     context "with IG by http URL #{url}" do
-      it 'matches http uri regex' do
-        expect(url).to match(Inferno::Utils::IgDownloader::HTTP_URI_REG_EX)
+      describe 'HTTP_URI_REG_EX' do
+        it 'matches given url' do
+          expect(url).to match(Inferno::Utils::IgDownloader::HTTP_URI_REG_EX)
+        end
       end
 
-      it 'does not match fhir package name regex' do
-        expect(url).to_not match(Inferno::Utils::IgDownloader::FHIR_PACKAGE_NAME_REG_EX)
+      describe 'FHIR_PACKAGE_NAME_REG_EX' do
+        it 'does not match given url' do
+          expect(url).to_not match(Inferno::Utils::IgDownloader::FHIR_PACKAGE_NAME_REG_EX)
+        end
       end
 
-      it 'normalizes to a package.tgz url' do
-        expect(ig_downloader.ig_http_url(url)).to match(%r{https?://build.fhir.org/ig/HL7/fhir-udap-security-ig/package.tgz})
+      describe '#ig_http_url' do
+        it 'normalizes to a package.tgz url' do
+          expect(ig_downloader.ig_http_url(url)).to match(%r{https?://build.fhir.org/ig/HL7/fhir-udap-security-ig/package.tgz})
+        end
       end
 
-      it 'downloads IG' do
-        stub_request(:get, %r{https?://build.fhir.org}).to_return(body: package_binary)
-        with_temp_path("ig-downloader-#{url}") do |temp_path|
-          allow(ig_downloader).to receive(:ig_file).and_return(temp_path)
-          ig_downloader.load_ig(url, nil, { verbose: false })
-          expect(File.read(temp_path)).to eq(package_binary)
+      describe '#load_ig' do
+        it 'downloads IG' do
+          stub_request(:get, %r{https?://build.fhir.org}).to_return(body: package_binary)
+          with_temp_path("ig-downloader-#{url}") do |temp_path|
+            allow(ig_downloader).to receive(:ig_file).and_return(temp_path)
+            ig_downloader.load_ig(url, nil, { verbose: false })
+            expect(File.read(temp_path)).to eq(package_binary)
+          end
         end
       end
     end
@@ -105,28 +123,39 @@ RSpec.describe Inferno::Utils::IgDownloader do
   context 'with IG by absolute file path' do
     let(:absolute_path) { "file://#{PACKAGE_FIXTURE}" }
 
-    it 'matches file regex' do
-      expect(absolute_path).to match(Inferno::Utils::IgDownloader::FILE_URI_REG_EX)
+    describe 'FILE_URI_REG_EX' do
+      it 'matches given file uri' do
+        expect(absolute_path).to match(Inferno::Utils::IgDownloader::FILE_URI_REG_EX)
+      end
     end
 
-    it 'does not match http uri regex' do
-      expect(absolute_path).to_not match(Inferno::Utils::IgDownloader::HTTP_URI_REG_EX)
+    describe 'HTTP_URI_REG_EX' do
+      it 'does not match given file uri' do
+        expect(absolute_path).to_not match(Inferno::Utils::IgDownloader::HTTP_URI_REG_EX)
+      end
     end
 
-    it 'does not match fhir package name regex' do
-      expect(absolute_path).to_not match(Inferno::Utils::IgDownloader::FHIR_PACKAGE_NAME_REG_EX)
+    describe 'FHIR_PACKAGE_NAME_REG_EX' do
+      it 'does not match given file uri' do
+        expect(absolute_path).to_not match(Inferno::Utils::IgDownloader::FHIR_PACKAGE_NAME_REG_EX)
+      end
     end
 
-    it 'downloads IG' do
-      with_temp_path('ig-downloader-file') do |temp_path|
-        allow(ig_downloader).to receive(:ig_file).and_return(temp_path)
-        ig_downloader.load_ig(absolute_path, nil, { verbose: false })
-        expect(File.read(temp_path)).to eq(package_binary)
+    describe '#load_ig' do
+      it 'downloads IG' do
+        with_temp_path('ig-downloader-file') do |temp_path|
+          allow(ig_downloader).to receive(:ig_file).and_return(temp_path)
+          ig_downloader.load_ig(absolute_path, nil, { verbose: false })
+          expect(File.read(temp_path)).to eq(package_binary)
+        end
       end
     end
   end
 
-  it 'with bad input raises exception' do
-    expect { ig_downloader.load_ig('bad') }.to raise_error(Inferno::Utils::IgDownloader::Error)
+  describe '#load_ig' do
+    it 'with bad input raises exception' do
+      expect { ig_downloader.load_ig('bad') }.to raise_error(Inferno::Utils::IgDownloader::Error)
+    end
+
   end
 end
