0d53119ae733664c228beb0b3298605d7ccc852e
Alyssa Wang <awang@mitre.org>
2024-03-29 12:56:35 -0400

    adjust styles

--- client/src/components/InputsModal/styles.tsx
+++ client/src/components/InputsModal/styles.tsx
@@ -8,6 +8,7 @@ export default makeStyles()((theme: Theme) => ({
     overflow: 'auto !important',
   },
   inputField: {
+    marginTop: '0 !important',
     '& > label.MuiInputLabel-shrink': {
       fontWeight: 600,
       color: theme.palette.common.grayDarkest,
