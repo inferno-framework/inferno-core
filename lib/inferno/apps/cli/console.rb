2e4d84cfb98ffc38ae40d463b0820880b8bfe388
Stephen MacVicar <smacvicar@mitre.org>
2024-05-31 13:29:00 -0400

    skip loading validator sessions when using inferno console

--- lib/inferno/apps/cli/console.rb
+++ lib/inferno/apps/cli/console.rb
@@ -4,6 +4,9 @@ module Inferno
       def run
         require_relative '../../../inferno'
 
+        ENV['ASYNC_JOBS'] = 'false'
+        ENV['INITIALIZE_VALIDATOR_SESSIONS'] = 'false'
+
         Inferno::Application.finalize!
         Pry.start
       end
