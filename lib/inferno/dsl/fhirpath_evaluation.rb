f7ecf4e6f6af298f651fc78a39b0aab60d41da82
Brian Meshell <bmeshell@mitre.org>
2024-07-22 08:39:47 -0400

    first attempt at writing an RSpec test for a fhirpath evaluator method

--- lib/inferno/dsl/fhirpath_evaluation.rb
+++ lib/inferno/dsl/fhirpath_evaluation.rb
@@ -1,17 +1,21 @@
 require_relative '../ext/fhir_models'
 
 module Inferno
-    module DSL
-        module FhirpathEvaluation
+  module DSL
+    module FhirpathEvaluation
+      class Evaluator 
 
-            # Evaluates a fhirpath expression for a given instance of a FHIR resource
-            #
-            # @param fhir_resource [FHIR] the instance of a FHIR resource to use when evaluating the fhirpath expression
-            # @param fhirpath_expression [String] the expression to evaluate for the given FHIR resource instance
-            def evaluate_fhirpath(fhir_resource, fhirpath_expression)
-                FHIRPath.evaluate(fhir_resource, fhirpath_expression)
-            end
-
+        def initialize()
         end
+
+        # Evaluates a fhirpath expression for a given instance of a FHIR resource
+        #
+        # @param fhir_resource [FHIR] the instance of a FHIR resource to use when evaluating the fhirpath expression
+        # @param fhirpath_expression [String] the expression to evaluate for the given FHIR resource instance
+        def evaluate_fhirpath(fhir_resource, fhirpath_expression)
+          FHIR::FHIRPath.evaluate(fhir_resource, fhirpath_expression)
+        end
+      end
     end
+  end
 end
--- spec/inferno/dsl/fhirpath_evaluation_spec.rb
+++ spec/inferno/dsl/fhirpath_evaluation_spec.rb
@@ -1,6 +1,20 @@
 require_relative '../../../lib/inferno/dsl/fhirpath_evaluation'
 RSpec.describe Inferno::DSL::FhirpathEvaluation do
-    it 'is my first RSpec test' do
-        expect([]).to be_empty #example that should always pass
-    end
+  
+  let(:evaluator) {Inferno::DSL::FhirpathEvaluation::Evaluator.new}
+
+  #patient example adapted from https://fhirpath-lab.azurewebsites.net/FhirPath/
+  let(:patient) { FHIR::Patient.new(
+    id: 'example',
+    name: [FHIR::HumanName.new(family: 'Chalmers', given: ['Peter', 'James'])],
+    gender: 'male',
+    birthdate: '1974-12-25'
+  ) }
+
+  it 'is tests evaluating a fhirpath expression against a known patient resource' do
+    birthdate_expression = 'Patient.birthdate'
+    expected_birthdate = '1974-12-25'
+    birthdate_result = evaluator.evaluate_fhirpath(patient, birthdate_expression)
+    expect(birthdate_result).to eq(expected_birthdate)
+  end
 end
