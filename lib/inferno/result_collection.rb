69fba7b120be228c9adf3edbc71225e1256ab5f7
Vanessa Fotso <vfotso@mitre.org>
2024-07-29 14:28:54 -0400

    Update to lookup results by full runnable id and partial id
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/inferno/result_collection.rb
+++ lib/inferno/result_collection.rb
@@ -62,8 +62,7 @@ module Inferno
     private
 
     def lookup_by_runnable_id(key)
-      key = "-#{key}"
-      results.find { |result| result.runnable&.id&.end_with?(key) }
+      results.find { |result| result.runnable&.id == key.to_s || result.runnable&.id&.end_with?("-#{key}") }
     end
   end
 end
