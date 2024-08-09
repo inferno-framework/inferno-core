2d6ae5a3b6f109165fc7d74395aeee63e886e9f4
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-08-09 18:11:35 -0400

    increase codecov

--- spec/inferno/utils/persist_inputs_spec.rb
+++ spec/inferno/utils/persist_inputs_spec.rb
@@ -35,5 +35,26 @@ RSpec.describe Inferno::Utils::PersistInputs do
 
       expect(persisted_data).to eq('persist me')
     end
+
+    it 'can handle unregistered inputs' do
+      test_session = test_sessions_repo.create(test_suite_id: suite.id)
+
+      test_run = create(:test_run, test_session:)
+      test_run.test_session_id = test_session.id
+
+      params = {
+        test_session_id: test_session.id,
+        test_suite_id: suite.id,
+        inputs: [
+          { name: 'unregistered', value: 'omit me' },
+          { name: 'input1', value: 'persist me' }
+        ]
+      }
+
+      expect { dummy.persist_inputs(session_data_repo, params, test_run) }.to_not raise_error
+
+      persisted_data = session_data_repo.load(test_session_id: test_run.test_session_id, name: 'input1')
+      expect(persisted_data).to eq('persist me')
+    end
   end
 end
