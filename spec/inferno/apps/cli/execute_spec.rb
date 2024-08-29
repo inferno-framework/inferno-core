d6fef8be6aaba987835cabf00c1f9712b6cbd50f
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-08-29 15:40:36 -0400

    execute_spec JSON parse validation

--- spec/inferno/apps/cli/execute_spec.rb
+++ spec/inferno/apps/cli/execute_spec.rb
@@ -133,9 +133,7 @@ RSpec.describe Inferno::CLI::Execute do # rubocop:disable RSpec/FilePath
     end
 
     it 'returns valid JSON' do
-      expect { JSON.parse(instance.serialize(test_results)) }.to_not raise_error(JSON::ParserError)
-      expect { JSON.parse(instance.serialize(test_results)) }.to_not raise_error(JSON::NestingError)
-      expect { JSON.parse(instance.serialize(test_results)) }.to_not raise_error(TypeError)
+      expect { JSON.parse(instance.serialize(test_results)) }.to_not raise_error(StandardError)
     end
   end
 
