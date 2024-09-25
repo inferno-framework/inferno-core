cd674c0e446dfe000d10a10472fbd8cff48926af
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-09-25 11:41:50 -0400

    remove exception raises from abstract methods in abstract outputter

--- lib/inferno/apps/cli/execute/abstract_outputter.rb
+++ lib/inferno/apps/cli/execute/abstract_outputter.rb
@@ -8,31 +8,26 @@ module Inferno
       class AbstractOutputter
         # @see Inferno::CLI::Main#execute for options
         def print_start_message(options)
-          raise StandardError, 'not implemented'
         end
 
         # Implementation MUST call `yield`
         # @see Inferno::CLI::Main#execute for options
         def print_around_run(options, &)
-          raise StandardError, 'not implemented'
         end
 
         # @see Inferno::CLI::Main#execute for options
         # @param results [Array<Inferno::Entity::Result>]
         def print_results(options, results)
-          raise StandardError, 'not implemented'
         end
 
         # @see Inferno::CLI::Main#execute for options
         def print_end_message(options)
-          raise StandardError, 'not implemented'
         end
 
         # Implementation MUST NOT re-raise exception or exit
         # @see Inferno::CLI::Main#execute for options
         # @param exception [Exception]
         def print_error(options, exception)
-          raise StandardError, 'not implemented'
         end
       end
       # rubocop:enable Lint/UnusedMethodArgument
