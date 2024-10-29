f956368c19cf825498bb50b771047247992cb81b
Vanessa Fotso <vfotso@mitre.org>
2024-10-29 13:16:51 -0400

    refactored in memorry repo
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/inferno/repositories/in_memory_repository.rb
+++ lib/inferno/repositories/in_memory_repository.rb
@@ -9,9 +9,10 @@ module Inferno
       def_delegators 'self.class', :all, :all_by_id
 
       def insert(entity)
-        raise Exceptions::DuplicateEntityIdException, entity.id if id_exists?(entity.id)
+        raise Exceptions::DuplicateEntityIdException, entity.id if exists?(entity.id)
 
         all << entity
+        all_by_id[entity.id.to_s] = entity
         entity
       end
 
@@ -20,7 +21,7 @@ module Inferno
       end
 
       def exists?(id)
-        all_by_id.include? id
+        all_by_id.key?(id.to_s)
       end
 
       class << self
@@ -31,20 +32,7 @@ module Inferno
         # @private
         def all_by_id
           @all_by_id ||= {}
-          @all_by_id.length == all.length ? @all_by_id : index_by_id
         end
-
-        def index_by_id
-          @all_by_id = {}
-          all.each { |klass| @all_by_id[klass.id] = klass }
-          @all_by_id
-        end
-      end
-
-      private
-
-      def id_exists?(id)
-        all.any? { |klass| klass.id == id }
       end
     end
   end
