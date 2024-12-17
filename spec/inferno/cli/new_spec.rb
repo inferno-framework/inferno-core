e037d7e2d773db1bd3ba164385d6feda5df3b1e9
Stephen MacVicar <smacvicar@mitre.org>
2024-12-17 12:10:32 -0500

    fix specs

--- spec/inferno/cli/new_spec.rb
+++ spec/inferno/cli/new_spec.rb
@@ -33,9 +33,9 @@ RSpec.describe Inferno::CLI::New do
       expect(File).to exist('test-fhir-app/lib/test_fhir_app.rb')
 
       if cli_args.count('--author') == 1
-        expect(File.read('test-fhir-app/lib/test_fhir_app/metadata.rb')).to include('authors ["ABC"]')
+        expect(File.read('test-fhir-app/lib/test_fhir_app/metadata.rb')).to include("authors ['ABC']")
       elsif cli_args.count('--author') == 2
-        expect(File.read('test-fhir-app/lib/test_fhir_app/metadata.rb')).to include('authors ["ABC", "DEF"]')
+        expect(File.read('test-fhir-app/lib/test_fhir_app/metadata.rb')).to include("authors ['ABC','DEF']")
       end
 
       if cli_args.count('--implementation-guide') == 1
