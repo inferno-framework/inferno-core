21ca8659874c27ae439c80b306e5f53157f30155
Shaumik-Ashraf <shaumikashraf@mitre.org>
2024-12-19 15:58:14 -0500

    clean

--- lib/inferno/apps/cli/suites.rb
+++ lib/inferno/apps/cli/suites.rb
@@ -6,8 +6,6 @@ module Inferno
       def run
         ENV['NO_DB'] = 'true'
 
-        # TODO delete me require_relative '../../../inferno'
-
         Inferno::Application.start(:suites)
 
         suites = Inferno::Repositories::TestSuites.new.all
