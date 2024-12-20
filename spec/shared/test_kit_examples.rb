8273d3934f286ff72c6bd72e7a8b9009b7420157
Stephen MacVicar <smacvicar@mitre.org>
2024-12-20 13:40:28 -0500

    WIP

--- spec/shared/test_kit_examples.rb
+++ spec/shared/test_kit_examples.rb
@@ -1,23 +1,29 @@
-Rspec.shared_examples 'deployable_test_kit' do
+RSpec.shared_examples 'deployable_test_kit' do
+  let(:test_kit_location) do
+    Object.const_source_location(described_class.name).first
+  end
+  let(:test_kit_gem) do
+    Bundler.definition.specs.find { |gem| test_kit_location.start_with? gem.full_gem_path }
+  end
+  let(:base_path) do
+    test_kit_gem.full_gem_path
+  end
+  let(:test_kit) do
+    described_class.const_get('Metadata')
+  rescue NameError
+    skip 'TestKit must be defined first'
+  end
+
   describe 'TestKit' do
-    let(:test_kit_location) do
-      Object.const_source_location(described_class.name)
-    end
-    let(:test_kit_gem) do
-      Bundler.definition.specs.find { |gem| test_kit_location.start_with? gem.full_gem_path }
-    end
-    let(:base_path) { test_git_gem.full_gem_path }
-    let(:test_kit) { described_class.const_get('Metadata') }
-
     it 'defines test kit in the Metadata class' do
-      error_message = <<~MESSAGE
-        Define #{described_class.name}::Metadata at
-        lib/#{described_class.name.underscore}/metadata.rb and require it in
-        lib/#{described_class.name.underscore}.rb
-      MESSAGE
-      expect { test_kit }.to_not raise(NameError), error_message
+      error_message =
+        "Define #{described_class.name}::Metadata at " \
+        "lib/#{described_class.name.underscore}/metadata.rb and require it in " \
+        "lib/#{described_class.name.underscore}.rb\n"
 
-      expect(test_kit).to be_a(Inferno::Entities::TestKit)
+      expect { described_class.const_get('Metadata') }.to_not raise_error(NameError), error_message
+
+      expect(described_class.const_get('Metadata')).to be_a(Inferno::Entities::TestKit)
     end
 
     it 'relies on the test kit version rather than defining the version in the suites' do
@@ -26,11 +32,10 @@ Rspec.shared_examples 'deployable_test_kit' do
       suite_contents = suite_paths.map { |path| File.read(path) }
 
       suite_contents.each_with_index do |suite, i|
-        error_message = <<~MESSAGE
-          Suite at #{suite_paths[i]} should not explicitly declare a version, as
-          its version can now be determined by the version of its Test Kit.
-          Remove the `version` method call in the suite definition.
-        MESSAGE
+        error_message =
+          "Suite at #{suite_paths[i]} should not explicitly declare a version, as " \
+          'its version can now be determined by the version of its Test Kit.' \
+          "Remove the `version` method call in the suite definition.\n"
 
         expect(suite).to_not match(%r{^\s+version(\s|\()\S+\)?}), error_message
       end
@@ -52,11 +57,10 @@ Rspec.shared_examples 'deployable_test_kit' do
     end
 
     it 'has a description with a <!-- break -->' do
-      error_message = <<~MESSAGE
-        The test kit description must begin with one paragraph followed by "<!--
-        break -->". The portion of the description above the break is displayed
-        on the test kit listing page.
-      MESSAGE
+      error_message =
+        'The test kit description must begin with one paragraph followed by "<!-- ' \
+        'break -->". The portion of the description above the break is displayed ' \
+        "on the test kit listing page.\n"
 
       expect(test_kit.description).to include('<!-- break -->'), error_message
     end
@@ -81,11 +85,10 @@ Rspec.shared_examples 'deployable_test_kit' do
     it 'uses git to determine files to include in the gem' do
       gemspec_contents = File.read(File.join(base_path, "#{test_kit_gem.name}.gemspec"))
 
-      error_message = <<~MESSAGE
-        Use git to determine which files to include in the gem. In
-        #{test_kit_gem.name}.gemspec, use:
-        spec.files = `[ -d .git ] && git ls-files -z lib config/presets LICENSE`.split("\x0")
-      MESSAGE
+      error_message =
+        'Use git to determine which files to include in the gem. In ' \
+        "#{test_kit_gem.name}.gemspec, use: " \
+        "spec.files = `[ -d .git ] && git ls-files -z lib config/presets LICENSE`.split(\"\\x0\")\n"
 
       expect(gemspec_contents).to include('[ -d .git ] && git ls-files'), error_message
     end
