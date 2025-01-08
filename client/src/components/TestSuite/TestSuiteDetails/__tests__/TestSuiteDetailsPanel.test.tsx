1f02d64887d2a054c9a500d8cc727b3e4db9b0b9
Alyssa Wang <awang@mitre.org>
2025-01-08 13:52:10 -0500

    update missing import

--- client/src/components/TestSuite/TestSuiteDetails/__tests__/TestSuiteDetailsPanel.test.tsx
+++ client/src/components/TestSuite/TestSuiteDetails/__tests__/TestSuiteDetailsPanel.test.tsx
@@ -5,7 +5,7 @@ import { SnackbarProvider } from 'notistack';
 import ThemeProvider from 'components/ThemeProvider';
 import TestSuiteDetailsPanel from '~/components/TestSuite//TestSuiteDetails/TestSuiteDetailsPanel';
 import { mockedTestSuite } from '~/components/_common/__mocked_data__/mockData';
-import { mockedRunTests } from '~/components/TestSuite/TestActionButton/__mocked_data__/mockData';
+import { mockedRunTests } from '~/components/TestSuite/TestRunButton/__mocked_data__/mockData';
 import { mockedRequestFunctions } from '~/components/RequestDetailModal/__mocked_data__/mockData';
 import { expect, test } from 'vitest';
 
