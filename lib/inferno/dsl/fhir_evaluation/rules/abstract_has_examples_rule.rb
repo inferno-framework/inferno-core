6934b21275b41fd0c6c6ec9158fdc70070c03c72
Jaehoon Lee <jaehoonlee@mitre.org>
2025-02-06 16:06:54 -0500

    Initial commit

---
+++ lib/inferno/dsl/fhir_evaluation/rules/abstract_has_examples_rule.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../reference_extractor'
+
+module Inferno
+  module DSL
+    module FHIREvaluation
+      module Rules
+        class HasExamples < Rule
+          def check(_context)
+            ''
+          end
+
+          def unused_resource_urls
+            @unused_resource_urls ||= []
+          end
+
+          def used_resources
+            @used_resources ||= []
+          end
+
+          def get_unused_resource_urls(ig_data, &resource_filter)
+            ig_data.each do |resource|
+              unused_resource_urls.push resource.url unless resource_filter.call(resource)
+            end
+          end
+        end
+      end
+    end
+  end
+end
