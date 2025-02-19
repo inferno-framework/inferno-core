25866a19ed115170696f88fa2bd1fdfef26e926d
Stephen MacVicar <smacvicar@mitre.org>
2025-02-19 10:27:46 -0500

    add ig files startup provider

---
+++ lib/inferno/config/boot/ig_files.rb
@@ -0,0 +1,26 @@
+require 'fileutils'
+
+Inferno::Application.register_provider(:ig_files) do
+  prepare do
+    target_container.start :logging
+
+    test_kit_gems = Bundler.definition.specs.select { |spec| spec.metadata['inferno_test_kit']&.casecmp? 'true' }
+
+    ig_files = test_kit_gems.flat_map do |test_kit|
+      Dir.glob(File.join(test_kit.full_gem_path, 'lib', '*', 'igs', '*.tgz'))
+    end
+
+    if test_kit_gems.none? { |gem| gem.full_gem_path == Dir.pwd }
+      ig_files += Dir.glob(File.join(Dir.pwd, 'lib', '*', 'igs', '*.tgz'))
+    end
+
+    if File.exist? File.join(Dir.pwd, 'data', 'igs')
+      ig_files.each do |source_file_path|
+        destination_file_path = File.join(Dir.pwd, 'data', 'igs', File.basename(source_file_path))
+        Inferno::Application['logger'].info("Copying #{File.basename(source_file_path)} to data/igs")
+        # FileUtils.rm_f(destination_file_path)
+        FileUtils.copy_file(source_file_path, destination_file_path)
+      end
+    end
+  end
+end
