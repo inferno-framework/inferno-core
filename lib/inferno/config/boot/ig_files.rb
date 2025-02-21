bee881853767ac7c163348b9e39e570fa46775c2
Stephen MacVicar <smacvicar@mitre.org>
2025-02-21 12:09:41 -0500

    update ig file loader to indicate source of ig files

--- lib/inferno/config/boot/ig_files.rb
+++ lib/inferno/config/boot/ig_files.rb
@@ -10,19 +10,34 @@ Inferno::Application.register_provider(:ig_files) do
 
     test_kit_gems = Bundler.definition.specs.select { |spec| spec.metadata['inferno_test_kit']&.casecmp? 'true' }
 
-    ig_files = test_kit_gems.flat_map do |test_kit|
-      Dir.glob(File.join(test_kit.full_gem_path, 'lib', '*', 'igs', '*.tgz'))
-    end
+    test_kit_ig_files = test_kit_gems.map do |test_kit|
+      files = Dir.glob(File.join(test_kit.full_gem_path, 'lib', '*', 'igs', '*.tgz'))
+      next if files.blank?
 
-    if test_kit_gems.none? { |gem| gem.full_gem_path == Dir.pwd }
-      ig_files += Dir.glob(File.join(Dir.pwd, 'lib', '*', 'igs', '*.tgz'))
+      {
+        test_kit_name: test_kit.name,
+        files:
+      }
+    end.compact
+
+    local_ig_files = Dir.glob(File.join(Dir.pwd, 'lib', '*', 'igs', '*.tgz'))
+
+    if local_ig_files.present? && test_kit_gems.none? { |gem| gem.full_gem_path == Dir.pwd }
+      test_kit_ig_files += {
+        test_kit_name: 'current project',
+        files: local_ig_files
+      }
     end
 
     if File.exist? File.join(Dir.pwd, 'data', 'igs')
-      ig_files.each do |source_file_path|
-        destination_file_path = File.join(Dir.pwd, 'data', 'igs', File.basename(source_file_path))
-        Inferno::Application['logger'].info("Copying #{File.basename(source_file_path)} to data/igs")
-        FileUtils.copy_file(source_file_path, destination_file_path, true)
+      test_kit_ig_files.each do |ig_files|
+        ig_files[:files].each do |source_file_path|
+          destination_file_path = File.join(Dir.pwd, 'data', 'igs', File.basename(source_file_path))
+          Inferno::Application['logger'].info(
+            "Copying #{File.basename(source_file_path)} to data/igs from #{ig_files[:test_kit_name]}"
+          )
+          FileUtils.copy_file(source_file_path, destination_file_path, true)
+        end
       end
     end
   end
