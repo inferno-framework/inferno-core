bc9234882fdcb949f636ad8a6ebab5c7bc8026a7
Vanessa Fotso <vfotso@mitre.org>
2025-05-01 12:07:43 -0400

    reverse change to skip inserting requirement if exists
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/inferno/repositories/requirements.rb
+++ lib/inferno/repositories/requirements.rb
@@ -33,7 +33,6 @@ module Inferno
 
         result.each do |raw_req|
           requirement = Entities::Requirement.new(raw_req)
-          next if exists?(requirement.id)
 
           insert(requirement)
         end
