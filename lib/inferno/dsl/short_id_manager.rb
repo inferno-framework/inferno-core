6ecbbc0e25c77d4787bbf2af7236c01e4bfde2b3
Vanessa Fotso <vfotso@mitre.org>
2025-05-05 12:56:23 -0700

    add short id manager
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

---
+++ lib/inferno/dsl/short_id_manager.rb
@@ -0,0 +1,42 @@
+module Inferno
+  module DSL
+    # This module manages and locks short IDs, ensuring that short IDs
+    # remain stable and do not change unexpectedly.
+    module ShortIDManager
+      def short_id_file_path
+        File.join('lib', "#{name.underscore}_short_id_map.yml")
+      end
+
+      # Loads and memoizes the short ID map from the YAML file.
+      #
+      # @return [Hash] mapping of runnable IDs to their locked short IDs
+      def short_id_map
+        @short_id_map ||= YAML.load_file(short_id_file_path)
+      end
+
+      # Assigns locked short IDs to all descendant runnables based on the short ID map.
+      #
+      # @return [void]
+      def assign_short_ids
+        all_descendants.each do |runnable|
+          new_short_id = short_id_map.fetch(runnable.id)
+          runnable.short_id(new_short_id)
+        rescue KeyError
+          Inferno::Application['logger'].warn("No short id defined for #{runnable.id}")
+        end
+      rescue Errno::ENOENT
+        Inferno::Application['logger'].warn('No short id map found')
+      end
+
+      # Builds and memoizes the current mapping of runnable IDs to their short IDs.
+      #
+      # @return [Hash] current short ID mapping
+      def current_short_id_map
+        @current_short_id_map ||=
+          all_descendants.each_with_object({}) do |runnable, mapping|
+            mapping[runnable.id] = runnable.short_id
+          end
+      end
+    end
+  end
+end
