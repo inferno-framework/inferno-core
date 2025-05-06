69dbc4be79444e8939e7ea7f8538c9cf27be1c61
Stephen MacVicar <smacvicar@mitre.org>
2025-05-06 13:51:53 -0400

    update requirements export description

--- lib/inferno/apps/cli/requirements.rb
+++ lib/inferno/apps/cli/requirements.rb
@@ -5,13 +5,10 @@ module Inferno
     class Requirements < Thor
       desc 'export_csv', 'Export a csv represantation of requirements from an excel file'
       long_desc <<~LONGDESC
-        TODO
-        Generates a template for creating an input preset for a Test Suite.
-
-        With -f option, the preset template is written to the specified
-        filename.
+        Creates CSV files for tested requirements and requirements which are not
+        planned to be tested based on the excel files located in
+        "lib/test_kit_name/requirements"
       LONGDESC
-      # option :filename, banner: '<filename>', aliases: [:f]
       def export_csv
         ENV['NO_DB'] = 'true'
         RequirementsExporter.new.run
