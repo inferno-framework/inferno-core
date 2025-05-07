cf1a8a09d130b49ada478dda9d135ef9d678f4c8
Alyssa Wang <awang@farsight.internal>
2025-05-07 19:39:10 -0400

    sort requirements by url

--- client/src/components/TestSuite/Requirements/RequirementContent.tsx
+++ client/src/components/TestSuite/Requirements/RequirementContent.tsx
@@ -1,16 +1,27 @@
 import React, { FC } from 'react';
-import { Box, Chip, Grid2, Link, Stack, Typography } from '@mui/material';
+import { Box, Chip, Divider, Grid2, Link, Stack, Typography } from '@mui/material';
 import { blue, grey, purple, red, teal } from '@mui/material/colors';
 import { Requirement } from '~/models/testSuiteModels';
-import { useTestSessionStore } from '~/store/testSession';
+// import { useTestSessionStore } from '~/store/testSession';
 
 interface RequirementContentProps {
   requirements: Requirement[];
 }
 
 const RequirementContent: FC<RequirementContentProps> = ({ requirements }) => {
-  const viewOnly = useTestSessionStore((state) => state.viewOnly);
-  const viewOnlyUrl = viewOnly ? '/view' : '';
+  const requirementsByUrl = requirements.reduce(
+    // Reduce list of requirements into map of url -> list of requirements
+    (acc, current) => {
+      const key = current.url ?? '';
+      if (acc[key]) acc[key].push(current);
+      else acc[key] = [current];
+      return acc;
+    },
+    {} as Record<string, Requirement[]>,
+  );
+  let requirementCount = 0;
+  // const viewOnly = useTestSessionStore((state) => state.viewOnly);
+  // const viewOnlyUrl = viewOnly ? '/view' : '';
 
   const conformanceChip = (text: string) => {
     const conformanceToColor: Record<string, string> = {
@@ -33,45 +44,56 @@ const RequirementContent: FC<RequirementContentProps> = ({ requirements }) => {
     );
   };
 
-  return requirements.map((requirement, index) => (
-    <Box key={requirement.id}>
-      <Typography>{requirement.url}</Typography>
-      <Grid2 container spacing={2} mb={2} key={index}>
-        <Grid2 size={{ xs: 4, sm: 3, md: 2 }}>
-          <Stack>
-            <Typography fontWeight="bold">Requirement {index + 1}:</Typography>
-            <Typography>{requirement.actor.toUpperCase()}</Typography>
-          </Stack>
-        </Grid2>
-        <Grid2 size="grow">
-          <Stack>
-            <Box px={1} mb={1} sx={{ borderLeft: `4px solid ${grey[100]}` }}>
-              <Typography>{requirement.requirement}</Typography>
-            </Box>
-            <Box display="flex" px={1.5} mb={requirement.sub_requirements.length > 0 ? 1 : 0}>
-              {conformanceChip(requirement.conformance)}
-              {/* <Typography ml={4} fontWeight="bold">
-                Test:{' '}
-                <Link href={`#${requirement.id}${viewOnlyUrl}`} color="secondary">
-                  {requirement.id}
-                </Link>
-              </Typography> */}
-            </Box>
-            {/* Subrequirements */}
-            {requirement.sub_requirements.length > 0 && (
-              <Box display="flex" px={1.5}>
-                <Typography>
-                  <b>{requirement.id}</b> Sub-requirements:{' '}
-                  {requirement.sub_requirements
-                    // Split sub-requirements on '@' to isolate number
-                    .map((subRequirement) => subRequirement.split('@').slice(-1))
-                    .join(', ')}
-                </Typography>
-              </Box>
-            )}
-          </Stack>
-        </Grid2>
-      </Grid2>
+  return Object.entries(requirementsByUrl).map(([url, requirementsList]) => (
+    <Box key={url}>
+      <Box pb={2}>
+        <Link href={url} color="secondary">
+          {url}
+        </Link>
+      </Box>
+      {requirementsList.map((requirement) => {
+        requirementCount++;
+        return (
+          <Grid2 container spacing={2} mb={2} key={requirementCount}>
+            <Grid2 size={{ xs: 4, sm: 3, md: 2 }}>
+              <Stack>
+                <Typography fontWeight="bold">Requirement {requirementCount}:</Typography>
+                <Typography>{requirement.actor.toUpperCase()}</Typography>
+              </Stack>
+            </Grid2>
+            <Grid2 size="grow">
+              <Stack>
+                <Box px={1} mb={1} sx={{ borderLeft: `4px solid ${grey[100]}` }}>
+                  <Typography>{requirement.requirement}</Typography>
+                </Box>
+                <Box display="flex" px={1.5} mb={requirement.sub_requirements.length > 0 ? 1 : 0}>
+                  {conformanceChip(requirement.conformance)}
+                  {/* <Typography ml={4} fontWeight="bold">
+                  Test:{' '}
+                  <Link href={`#${requirement.id}${viewOnlyUrl}`} color="secondary">
+                    {requirement.id}
+                  </Link>
+                </Typography> */}
+                </Box>
+                {/* Subrequirements */}
+                {requirement.sub_requirements.length > 0 && (
+                  <Box display="flex" px={1.5}>
+                    <Typography>
+                      <b>{requirement.id}</b> Sub-requirements:{' '}
+                      {requirement.sub_requirements
+                        // Split sub-requirements on '@' to isolate number
+                        .map((subRequirement) => subRequirement.split('@').slice(-1))
+                        .join(', ')}
+                    </Typography>
+                  </Box>
+                )}
+              </Stack>
+            </Grid2>
+          </Grid2>
+        );
+      })}
+      {/* Empty URL is always last */}
+      {url && <Divider />}
     </Box>
   ));
 };
