c1013c4da3dd577e375c4b525516f107cbf99399
Vanessa Fotso <46642178+vanessuniq@users.noreply.github.com>
2025-05-20 11:49:57 -0400

    FI-3816 Lock Short IDs (#677)
    * add runnable method to get all descendants
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * update short_id method to take an optional arg
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * add short id manager
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * add cli command to persist current short id map for a suite
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * add shareable unit tests for short_id_map
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * add example usage for
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * lock short id at boot time
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * update logic for short_id_map path
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * quietly skip if short id map not present
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    ---------
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

---
+++ lib/inferno/dsl/short_id_manager.rb
@@ -0,0 +1,55 @@
+module Inferno
+  module DSL
+    # This module manages and locks short IDs, ensuring that short IDs
+    # remain stable and do not change unexpectedly.
+    module ShortIDManager
+      def base_short_id_file_folder
+        File.join(Dir.pwd, 'lib', name.split('::').first.underscore)
+      end
+
+      def short_id_file_name
+        "#{name.demodulize.underscore}_short_id_map.yml"
+      end
+
+      def short_id_file_path
+        File.join(base_short_id_file_folder, short_id_file_name).freeze
+      end
+
+      # Loads and memoizes the short ID map from the YAML file.
+      #
+      # @return [Hash] mapping of runnable IDs to their locked short IDs
+      def short_id_map
+        return unless File.exist?(short_id_file_path)
+
+        @short_id_map ||= YAML.load_file(short_id_file_path)
+      end
+
+      # @private
+      # Assigns locked short IDs to all descendant runnables based on the short ID map.
+      #
+      # This method is called at boot time.
+      #
+      # @return [void]
+      def assign_short_ids
+        return unless short_id_map
+
+        all_descendants.each do |runnable|
+          new_short_id = short_id_map.fetch(runnable.id)
+          runnable.short_id(new_short_id)
+        rescue KeyError
+          Inferno::Application['logger'].warn("No short id defined for #{runnable.id}")
+        end
+      end
+
+      # Builds and memoizes the current mapping of runnable IDs to their short IDs.
+      #
+      # @return [Hash] current short ID mapping
+      def current_short_id_map
+        @current_short_id_map ||=
+          all_descendants.each_with_object({}) do |runnable, mapping|
+            mapping[runnable.id] = runnable.short_id
+          end
+      end
+    end
+  end
+end
