c1dbd65e2c6b18247c00b75e98c4b2a0b886add2
Stephen MacVicar <smacvicar@mitre.org>
2025-06-02 14:09:18 -0400

    fix documentation indentation

--- lib/inferno/dsl/suite_requirements.rb
+++ lib/inferno/dsl/suite_requirements.rb
@@ -9,33 +9,33 @@ module Inferno
       # @return [Array<Inferno::DSL::RequirementSet>]
       #
       # @example
-      # class Suite < Inferno::TestSuite
-      #   requirement_sets(
-      #     {
-      #       identifier: 'example-regulation-1',
-      #       title: 'Example Regulation 1',
-      #       actor: 'Provider' # Only include requirements for the 'Provider'
-      #                         # actor
-      #     },
-      #     {
-      #       identifier: 'example-ig-1',
-      #       title: 'Example Implementation Guide 1',
-      #       actor: 'Provider',
-      #       requirements: '2, 4-5' # Only include these specific requirements
-      #     },
-      #     {
-      #       identifier: 'example-ig-2',
-      #       title: 'Example Implementation Guide 2',
-      #       requirements: 'Referenced', # Only include requirements from this
-      #                                   # set that are referenced by other
-      #                                   # included requirements
-      #       actor: 'Server',
-      #       suite_options: {      # Only include these requirements if the ig
-      #         ig_version: '3.0.0' # version 3.0.0 suite option is selected
+      #   class Suite < Inferno::TestSuite
+      #     requirement_sets(
+      #       {
+      #         identifier: 'example-regulation-1',
+      #         title: 'Example Regulation 1',
+      #         actor: 'Provider' # Only include requirements for the 'Provider'
+      #                           # actor
+      #       },
+      #       {
+      #         identifier: 'example-ig-1',
+      #         title: 'Example Implementation Guide 1',
+      #         actor: 'Provider',
+      #         requirements: '2, 4-5' # Only include these specific requirements
+      #       },
+      #       {
+      #         identifier: 'example-ig-2',
+      #         title: 'Example Implementation Guide 2',
+      #         requirements: 'Referenced', # Only include requirements from this
+      #                                     # set that are referenced by other
+      #                                     # included requirements
+      #         actor: 'Server',
+      #         suite_options: {      # Only include these requirements if the ig
+      #           ig_version: '3.0.0' # version 3.0.0 suite option is selected
+      #         }
       #       }
-      #     }
-      #   )
-      # end
+      #     )
+      #   end
       def requirement_sets(*sets)
         @requirement_sets = sets.map { |set| RequirementSet.new(**set) } if sets.present?
 
