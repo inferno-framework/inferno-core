a5f210ad7792fe30a29d3b9a475922e1973d1273
Stephen MacVicar <smacvicar@mitre.org>
2025-06-16 13:25:30 -0400

    add newline for each test suite in coverage check command

--- lib/inferno/apps/cli/requirements_coverage_checker.rb
+++ lib/inferno/apps/cli/requirements_coverage_checker.rb
@@ -165,6 +165,8 @@ module Inferno
           exit(1)
         end
 
+        puts
+
         if unmatched_requirement_ids.any?
           puts "WARNING: The following requirements indicated in the test suite #{test_suite_id} are not present in " \
                "the suite's requirement sets:"
