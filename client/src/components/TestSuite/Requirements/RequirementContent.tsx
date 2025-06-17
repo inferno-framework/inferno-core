8281b6fc1d217b73694316145386a8759917c1fd
Alyssa Wang <awang@farsight.internal>
2025-06-17 11:30:57 -0400

    add distinct specification blocks

--- client/src/components/TestSuite/Requirements/RequirementContent.tsx
+++ client/src/components/TestSuite/Requirements/RequirementContent.tsx
@@ -10,23 +10,34 @@ import useStyles from './styles';
 
 interface RequirementContentProps {
   requirements: Requirement[];
+  view: 'page' | 'dialog';
   requirementToTests?: Map<string, string[]>;
 }
 
-const RequirementContent: FC<RequirementContentProps> = ({ requirements, requirementToTests }) => {
+const RequirementContent: FC<RequirementContentProps> = ({
+  requirements,
+  view = 'page',
+  requirementToTests,
+}) => {
   const { classes } = useStyles();
-  const requirementsByUrl = requirements.reduce(
-    // Reduce list of requirements into map of url -> list of requirements
-    (acc, current) => {
-      const key = current.url ?? '';
-      if (acc[key]) acc[key].push(current);
-      else acc[key] = [current];
-      return acc;
-    },
-    {} as Record<string, Requirement[]>,
-  );
   const viewOnly = useTestSessionStore((state) => state.viewOnly);
   const viewOnlyUrl = viewOnly ? '/view' : '';
+
+  // Reduce list of requirements into map of specification -> url -> list of requirements
+  const requirementsByUrl = requirements.reduce(
+    (acc, current) => {
+      const specification = current.id.split('@')[0];
+      const url = current.url ?? '';
+      if (acc[specification]) {
+        if (acc[specification][url]) acc[specification][url].push(current);
+        else acc[specification][url] = [current];
+      } else {
+        acc[specification] = { [url]: [current] };
+      }
+      return acc;
+    },
+    {} as Record<string, Record<string, Requirement[]>>,
+  );
 
   // Nested components
   const conformanceChip = (text: string) => {
@@ -50,24 +61,35 @@ const RequirementContent: FC<RequirementContentProps> = ({ requirements, require
     );
   };
 
-  /* Subrequirements and test links */
-  const requirementDetails = (requirement: Requirement) => {
-    const testLinksExist = requirementToTests !== undefined && requirementToTests !== null;
+  const testLinks = (requirement: Requirement) => {
+    const testLinksExist = requirementToTests && requirementToTests.size > 0;
     const testIds = requirementToTests?.get(requirement.id);
-    // If there are no testIds, then show nothing
-    if (testLinksExist && testIds) {
+    // If view is 'dialog,' then show nothing
+    if (testLinksExist && testIds && view === 'page') {
       return (
         <Box display="flex" px={1.5}>
           <Typography ml={0} variant="body2" fontWeight="bold">
             Test:{' '}
-            {testIds?.map((id, i) => (
-              <span key={id}>
-                <Link variant="body2" href={`#${id}${viewOnlyUrl}`} color="secondary">
-                  {id}
-                </Link>
-                {i !== testIds.length - 1 ? ', ' : ''} {/* Separate values with commas */}
-              </span>
-            ))}
+            {testIds?.map((id, i) => {
+              return (
+                <span key={id}>
+                  {id !== 'Not tested' ? (
+                    <Link variant="body2" href={`#${id}${viewOnlyUrl}`} color="secondary">
+                      {id}
+                    </Link>
+                  ) : (
+                    <Typography
+                      ml={0}
+                      variant="body2"
+                      sx={{ color: lightTheme.palette.common.orangeDark }}
+                    >
+                      Not tested
+                    </Typography>
+                  )}
+                  {i !== testIds.length - 1 ? ', ' : ''} {/* Separate values with commas */}
+                </span>
+              );
+            })}
           </Typography>
         </Box>
       );
@@ -82,45 +104,47 @@ const RequirementContent: FC<RequirementContentProps> = ({ requirements, require
     }
   };
 
-  return Object.entries(requirementsByUrl).map(([url, requirementsList], index) => (
-    <Box key={url}>
-      <Box>
-        {requirementsList[0] && (
-          <Typography variant="h5" component="p" fontWeight="bold" sx={{ mt: 1 }}>
-            {/* Pull the specification out from the first requirement */}
-            {requirementsList[0].id.split('@')[0]}
-          </Typography>
-        )}
-        {url ? (
-          <Link href={url} color="secondary">
-            {url}
-          </Link>
-        ) : (
-          <Typography color={lightTheme.palette.common.gray}>(no link available)</Typography>
-        )}
-      </Box>
-      {requirementsList.map((requirement) => (
-        <Grid2 container spacing={2} mt={2} key={requirement.id}>
-          <Grid2>
-            <Typography fontWeight="bold">{requirement.id.split('@').slice(-1)}</Typography>
-          </Grid2>
-          <Grid2>{conformanceChip(requirement.conformance)}</Grid2>
-          <Grid2 size="grow">
-            <Stack>
-              <Box px={1} mb={1} sx={{ borderLeft: `4px solid ${grey[100]}` }}>
-                <Markdown remarkPlugins={[remarkGfm]} className={classes.markdown}>
-                  {requirement.requirement}
-                </Markdown>
-              </Box>
-              {requirementDetails(requirement)}
-            </Stack>
-          </Grid2>
-        </Grid2>
-      ))}
-      {/* No divider if last section */}
-      {index !== Object.keys(requirementsByUrl).length - 1 && <Divider sx={{ mt: 2 }} />}
-    </Box>
-  ));
+  return Object.entries(requirementsByUrl).map(
+    ([specification, urlToRequirementsList], specificationIndex) =>
+      Object.entries(urlToRequirementsList).map(([url, requirementsList]) => (
+        <Box key={`${specification}-${url}`}>
+          <Box>
+            <Typography variant="h5" component="p" fontWeight="bold" sx={{ mt: 1 }}>
+              {specification}
+            </Typography>
+            {url ? (
+              <Link href={url} color="secondary">
+                {url}
+              </Link>
+            ) : (
+              <Typography color={lightTheme.palette.common.gray}>(no link available)</Typography>
+            )}
+          </Box>
+          {requirementsList.map((requirement) => (
+            <Grid2 container spacing={2} mt={2} key={requirement.id}>
+              <Grid2>
+                <Typography fontWeight="bold">{requirement.id.split('@').slice(-1)}</Typography>
+              </Grid2>
+              <Grid2>{conformanceChip(requirement.conformance)}</Grid2>
+              <Grid2 size="grow">
+                <Stack>
+                  <Box px={1} mb={1} sx={{ borderLeft: `4px solid ${grey[100]}` }}>
+                    <Markdown remarkPlugins={[remarkGfm]} className={classes.markdown}>
+                      {requirement.requirement}
+                    </Markdown>
+                  </Box>
+                  {testLinks(requirement)}
+                </Stack>
+              </Grid2>
+            </Grid2>
+          ))}
+          {/* No divider if last section */}
+          {specificationIndex !== Object.keys(requirementsByUrl).length - 1 && (
+            <Divider sx={{ mt: 2 }} />
+          )}
+        </Box>
+      )),
+  );
 };
 
 export default RequirementContent;
