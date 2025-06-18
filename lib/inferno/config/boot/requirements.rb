b083c32d9bc33586d80d28de55dc0ce4d543d750
Stephen MacVicar <smacvicar@mitre.org>
2025-06-18 08:10:50 -0400

    load requirements when lib directory does not match test kit id

--- lib/inferno/config/boot/requirements.rb
+++ lib/inferno/config/boot/requirements.rb
@@ -12,19 +12,24 @@ Inferno::Application.register_provider(:requirements) do
         .specs
         .select { |spec| spec.metadata.fetch('inferno_test_kit', 'false').casecmp? 'true' }
 
-    files_to_load = Dir.glob(['lib/*test_kit/requirements/*.csv'])
+    files_to_load = Dir.glob(['lib/*/requirements/*.csv'])
 
     if ENV['LOAD_DEV_SUITES'].present?
       ENV['LOAD_DEV_SUITES'].split(',').map(&:strip).reject(&:empty?).each do |suite|
-        files_to_load.concat Dir.glob(File.join(Inferno::Application.root, 'dev_suites', suite, 'requirements',
-                                                '*.csv'))
+        files_to_load.concat(
+          Dir.glob(
+            File.join(
+              Inferno::Application.root, 'dev_suites', suite, 'requirements', '*.csv'
+            )
+          )
+        )
       end
     end
 
     files_to_load +=
       test_kit_gems.flat_map do |gem|
         [
-          Dir.glob([File.join(gem.full_gem_path, 'lib', '*test_kit', 'requirements', '*.csv')])
+          Dir.glob([File.join(gem.full_gem_path, 'lib', '*', 'requirements', '*.csv')])
         ].flatten
       end
 
