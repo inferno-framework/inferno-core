81913c96723b8e3b4233dbf4ae96369056d63a1f
Stephen MacVicar <smacvicar@mitre.org>
2025-07-17 13:20:00 -0400

    filter requirements in group serializer

--- lib/inferno/apps/web/serializers/test_group.rb
+++ lib/inferno/apps/web/serializers/test_group.rb
@@ -21,18 +21,28 @@ module Inferno
         end
         field :test_groups do |group, options|
           suite_options = options[:suite_options]
-          TestGroup.render_as_hash(group.groups(suite_options), suite_options:)
+          suite_requirement_ids = options[:suite_requirement_ids]
+          TestGroup.render_as_hash(group.groups(suite_options), suite_options:, suite_requirement_ids:)
         end
         field :tests do |group, options|
           suite_options = options[:suite_options]
-          Test.render_as_hash(group.tests(suite_options), suite_options:)
+          suite_requirement_ids = options[:suite_requirement_ids]
+          Test.render_as_hash(group.tests(suite_options), suite_options:, suite_requirement_ids:)
         end
         field :inputs do |group, options|
           suite_options = options[:suite_options]
           Input.render_as_hash(group.available_inputs(suite_options).values)
         end
         field :output_definitions, name: :outputs, extractor: HashValueExtractor
-        field :verifies_requirements, if: :field_present?
+        field :verifies_requirements, if: :field_present? do |group, options|
+          if options[:suite_requirement_ids].blank?
+            group.verifies_requirements
+          else
+            group.verifies_requirements.select do |requirement_id|
+              group_requirement_ids.include? requirement_id
+            end
+          end
+        end
       end
     end
   end
